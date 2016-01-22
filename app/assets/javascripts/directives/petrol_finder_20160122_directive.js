fd4f58c858317116b01370045e9b4cd18da5e6f8
LKhadka <lawrence_2009@hotmail.com>
2016-01-22 13:01:00 +0545

    Updated petrol finder

---
+++ app/assets/javascripts/directives/petrol_finder_20160122_directive.js
@@ -0,0 +1,56 @@
+var app = angular.module('nepal');
+app.directive('petrolFinder', function() {
+    function link(scope) {
+        var map = L.map('map_20160122').setView([27.6838451, 85.355752], 10);
+        var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink,
+            maxZoom: 18
+        }).addTo(map);
+        map._initPathRoot();
+        var svg = d3.select("#map_20160122").select("svg");
+        var g = svg.append("g");
+        d3.json('data_20160122.json', function (collection) {
+            collection.objects.forEach(function(d){
+                d.LatLng = new L.LatLng(d.circle.coordinates[0],
+                    d.circle.coordinates[1])
+            });
+            var feature = g.selectAll("circle")
+                .data(collection.objects)
+                .enter()
+                .append("circle")
+                .style("stroke","black")
+                .style("fill", "#90C3D4")
+                .attr('fill-opacity', 1)
+                .attr('class', 'circles')
+                .on("mouseover", function(){
+                    d3.select(this).transition().attr('r', 18)
+                })
+                .on("click", function(d) {
+                    window.open(d.url,'_blank');
+                })
+                .on("mouseout", function() {
+                    d3.select(this).transition().attr('r', 10).attr('fill', '#90C3D4')
+                })
+                .attr("r", 10);
+
+            map.on("viewreset", update);
+            update();
+
+            function update() {
+                feature.attr("transform",
+                    function(d) {
+                        return "translate("+
+                            map.latLngToLayerPoint(d.LatLng).x +","+
+                            map.latLngToLayerPoint(d.LatLng).y +")";
+                    }
+                )
+            }
+        });
+        $('.leaflet-container').css('cursor','crosshair');
+    }
+    return {
+        link: link,
+        restrict: 'E'
+    }
+});
