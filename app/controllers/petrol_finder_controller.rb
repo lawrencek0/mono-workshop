cbaab39eff3fa213661360da69982ab25a4bf9fd
LKhadka <lawrence_2009@hotmail.com>
2016-01-23 17:04:58 +0545

    Removed respond_to json

--- app/controllers/petrol_finder_controller.rb
+++ app/controllers/petrol_finder_controller.rb
@@ -1,10 +1,4 @@
 class PetrolFinderController < ApplicationController
-  respond_to :json, :html
   def index
-
-    respond_to do |format|
-      format.html {}
-      format.json {}
-    end
   end
 end
