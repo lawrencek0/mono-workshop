d0952ebb9c6ee488837d3d7786f35bf5ab905754
LKhadka <lawrence_2009@hotmail.com>
2016-03-14 17:17:42 +0545

    add chart

--- app/pods/components/power-bar-chart/component.js
+++ app/pods/components/power-bar-chart/component.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    Ember.run.scheduleOnce("afterRender", function() {
+      var chart = c3.generate({
+        bindto: "#power-bar-chart",
+        data: {
+          columns: [
+            ['Power Consumption', 1292, 1500, 1589, 1844],
+            ['Power Generation', 762, 327, 1583, 542]
+          ],
+          type: 'bar'
+        },
+        axis: {
+          y: {
+            label: {
+              text: 'in MW',
+              position: 'outer-middle'
+            }
+          },
+          x: {
+            show: false
+          }
+        }
+      });
+    });
+  }
 });
