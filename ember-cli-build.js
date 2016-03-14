2b8d95a5b6f3ab89db702a6eabe9e11680207b66
LKhadka <lawrence_2009@hotmail.com>
2016-03-14 11:03:03 +0545

    import d3 and create about route

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,6 +25,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+  app.import('bower_components/d3/d3.js');
 
   return app.toTree();
 };
