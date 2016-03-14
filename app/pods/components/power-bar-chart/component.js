4d597fc55f26155e4d0a50c53f44bdd0db9b7d9f
LKhadka <lawrence_2009@hotmail.com>
2016-03-14 17:08:19 +0545

    add c3 bar chart data

--- app/pods/components/power-bar-chart/component.js
+++ app/pods/components/power-bar-chart/component.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  data: {
+    columns: [
+      ['Power Consumption', 1292, 1500, 1589, 1844],
+      ['Power Generation', 762, 327, 1583, 542]
+    ],
+    type: 'bar'
+  },
+  axis: {
+    y: {
+      label: {
+        text: 'in MW',
+        position: 'outer-middle'
+      }
+    },
+    x: {
+      show: false // ADD
+    }
+  },
+  bar: {
+    title: "I will give one soon"
+  }
 });
