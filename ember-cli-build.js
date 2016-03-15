2465f419c2a08024fae733826c29098fdcdc5e96
LKhadka <lawrence_2009@hotmail.com>
2016-03-15 08:28:50 +0545

    remove fingerprinting and add ember-leaflet

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,15 +9,6 @@ module.exports = function(defaults) {
       includePaths: [
         'bower_components/bootstrap-sass/assets/stylesheets'
       ]
-    },
-    fingerprint: {
-      exclude: [
-        'images/layers-2x.png',
-        'images/layers.png',
-        'images/marker-icon-2x.png',
-        'images/marker-icon.png',
-        'images/marker-shadow.png'
-      ]
     }
   });
 
