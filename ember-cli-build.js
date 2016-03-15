b74ec800a8232ea6d8aa04891ad430ee4a88d8af
LKhadka <lawrence_2009@hotmail.com>
2016-03-15 08:26:03 +0545

    remove ember-leaflet

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,6 +9,15 @@ module.exports = function(defaults) {
       includePaths: [
         'bower_components/bootstrap-sass/assets/stylesheets'
       ]
+    },
+    fingerprint: {
+      exclude: [
+        'images/layers-2x.png',
+        'images/layers.png',
+        'images/marker-icon-2x.png',
+        'images/marker-icon.png',
+        'images/marker-shadow.png'
+      ]
     }
   });
 
