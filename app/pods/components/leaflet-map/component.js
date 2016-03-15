c29d5aab120fe098332a28bdb35db9ecd3b28865
LKhadka <lawrence_2009@hotmail.com>
2016-03-15 09:17:58 +0545

    remove leaflet, ember-cli-mapbox and ember-cli-mapbox-map

--- app/pods/components/leaflet-map/component.js
+++ app/pods/components/leaflet-map/component.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement: function() {
-    Ember.run.scheduleOnce('afterRender', function () {
-      L.mapbox.accessToken = 'pk.eyJ1IjoibGtoYWRrYSIsImEiOiJjaWxzdTFlZ3MwMDdodTlrcjR6N3lwdTFtIn0._4d4rk09nzc6A-6MAY26Ew';
-      var map = L.mapbox.map('map', 'mapbox.streets')
-        .setView([40, -74.50], 9);
-    });
-  }
 });
