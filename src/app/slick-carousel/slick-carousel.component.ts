05023c4323cf5d096412272c797b07a3a79da3ce
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-02-20 21:47:47 -0600

    create component for carousel and change section

---
+++ src/app/slick-carousel/slick-carousel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hero-slick-carousel',
+  template: `
+    <p>
+      slick-carousel Works!
+    </p>
+  `,
+  styles: []
+})
+export class SlickCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
