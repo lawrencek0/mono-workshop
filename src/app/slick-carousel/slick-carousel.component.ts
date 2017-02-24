c9ce1f283b6f647df2bf14781dc807460aecf980
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-02-23 19:30:34 -0600

    add project images and urls

--- src/app/slick-carousel/slick-carousel.component.ts
+++ src/app/slick-carousel/slick-carousel.component.ts
@@ -9,8 +9,13 @@ import 'slick-carousel/slick/slick';
 })
 export class SlickCarouselComponent implements AfterViewInit {
   links = [
-    {name: 'Nepal Portal', url: 'https://nepalportal.herokuapp.com', image: 'v1487888597/Nepal%20Portal'},
-    {name: 'Narayan Gopal Tribute Page', url: 'https://codepen.io/LKhadka/full/vXQdZv', image: 'v1459667037/41saJId_rbkysj'}
+    {name: 'Nepal Portal', url: 'nepalportal.herokuapp.com', image: 'v1487888597/Nepal%20Portal'},
+    {name: 'Javascript Calculator', url: 'codepen.io/LKhadka/full/vXQdZv', image: 'v1487888083/Javascript%20Calculator'},
+    {name: 'Random Quote Generator', url: 'codepen.io/LKhadka/full/JXOmdB', image: 'v1487888072/Random%20Quote%20Generator'},
+    {name: 'Pomodoro Clock', url:'codepen.io/LKhadka/full/pNvRze', image:'v1487888046/Pomodoro%20Clock'},
+    {name: 'Weather App', url:'codepen.io/LKhadka/full/grodqq/', image: 'v1487888041/Weather%20App'},
+    {name: 'Wikipedia Viewer', url:'codepen.io/LKhadka/full/yOKKjv/', image:'v1487888044/Wikipedia%20Viewer'},
+    {name: 'Narayan Gopal Tribute Page', url:'codepen.io/LKhadka/full/pyWPwL/', image:'v1487888039/Narayan%20Gopal%20Tribute%20Page'}
     ];
 
   constructor() { }
