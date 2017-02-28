3f4772f7e675c205ef415efeeeecec7559bd0566
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-02-27 18:02:01 -0600

    revert back to basic auth

--- server/routes/contact.ts
+++ server/routes/contact.ts
@@ -1,12 +1,30 @@
 import { Router, Request, Response } from 'express';
+import * as nodemailer from 'nodemailer';
+
+import * as config from './config';
 
 const contactRouter: Router = Router();
 
+let transporter = nodemailer.createTransport( {
+  service: 'Gmail',
+  auth: {
+      user: config.mailUser,
+      pass: config.mailPass
+  }
+});
+
 contactRouter.post('/', (req: Request, res: Response) => {
-  res.json({
-    'message': 'Got yo data',
-    'res': req.body
-  })
+  let mailOptions = {
+    from: req.body.email,
+    to: 'lawatlifecompany@gmail.com',
+    subject: req.body.name,
+    text: req.body.message
+  };
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
 
 export { contactRouter }
