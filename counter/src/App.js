2b564540ac856a398030e09f455e5cc3cd3b7b20
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-02 11:56:08 -0500

    implement basic counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  };
+
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
   render() {
     return(
       <div>
-        <h2>Hello World</h2>
+        <h2>Counter: {this.state.counter}</h2>
+        <div>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        </div>
       </div>
     )
   }
