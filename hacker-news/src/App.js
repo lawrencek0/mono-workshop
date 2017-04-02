0d9447d4d3d3b92ad645e4864ad036536eb78d03
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-02 15:56:20 -0500

    add hot reloading

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to React';
     return (
       <div>
-        <h2>{helloWorld}</h2>
       </div>
     );
   }
