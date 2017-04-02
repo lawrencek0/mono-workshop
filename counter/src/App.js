b0849a3db473dc149f52a58164cf1651050c0613
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-02 12:31:24 -0500

    style app

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -26,11 +27,23 @@ class App extends Component {
 
   render() {
     return(
-      <div>
-        <h2>Counter: {this.state.counter}</h2>
-        <div>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6 offset-sm-3">
+            <div className="card">
+              <div className="card-block">
+                <h4 className="card-title text-center">Counter: {this.state.counter}</h4>
+                  <div className="row">
+                    <div className="col-sm-6 pull-left">
+                      <button className="btn btn-success" onClick={this.increment}>+</button>
+                    </div>
+                    <div className="col-sm-6 pull-right">
+                      <button className="btn btn-danger right" onClick={this.decrement}>-</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
