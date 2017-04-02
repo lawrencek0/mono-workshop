077e6b2e8ce47b86a75f9544caf06cd330dec61c
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-02 15:03:07 -0500

    project init

---
+++ hacker-news/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to React';
+    return (
+      <div>
+        <h2>{helloWorld}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
