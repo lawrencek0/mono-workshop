79ae8215fe4408f9d8f27b23a5e9c57ece2ab882
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-03 17:57:36 -0500

    refactor components to functional stateless components

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -22,48 +22,49 @@ const list = [
 const isSearched = (searchTerm) => (item) =>
   !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-        <form>
-          <input
-            type="text"
-            value={value}
-            onChange={onChange}
-          />
-        </form>
-    )
-  }
-}
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children}
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map((item) => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        ))}
+
+
+const Table = ({list, pattern, onDismiss}) =>
+  <div>
+    {list.filter(isSearched(pattern)).map((item) => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
-    )
-  }
-}
+    ))}
+  </div>
+
+
+const Button = ({onClick, className = '', children}) =>
+  <button
+    className={className}
+    onClick={onClick}
+    type="button"
+  >
+    {children}
+  </button>
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -96,8 +97,17 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div>
-        <Search value={searchTerm} onChange={this.onSearchChange} />
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
