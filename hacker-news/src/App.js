057ff532a0e12fe0e3a8f9a02c42d0ec4072c9b7
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-03 17:38:39 -0500

    split into components

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -19,11 +19,30 @@ const list = [
   },
 ];
 
-class App extends Component {
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class Search extends Component {
   render() {
+    const { value, onChange } = this.props;
+    return (
+        <form>
+          <input
+            type="text"
+            value={value}
+            onChange={onChange}
+          />
+        </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, pattern, onDismiss} = this.props;
     return (
       <div>
-        {list.map((item) => (
+        {list.filter(isSearched(pattern)).map((item) => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -31,8 +50,54 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
+      </div>
+    )
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onDismiss =this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+
+  }
+
+  onSearchChange(e) {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({
+      list: updatedList
+    });
+  }
+
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div>
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
