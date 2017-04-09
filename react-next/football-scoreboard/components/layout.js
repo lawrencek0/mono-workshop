5835f4b827ff8eb53992421e047a643bec2b4a9a
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-09 13:29:18 -0500

    setup project with react-md

--- react-next/football-scoreboard/components/layout.js
+++ react-next/football-scoreboard/components/layout.js
@@ -7,12 +7,19 @@ export default ({ children, title}) => (
       <title>{ title } Football Scoreboard</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
+      <style>{`
+        @import url(https://fonts.googleapis.com/css?family=Roboto:300,400,500);
+
+        body {
+          font-family: 'Roboto', sans-serif;
+        }
+      `}
+      </style>
     </Head>
     <header>
       <nav>
-        <Link href="/"><a>Home</a></Link>
+        <Link href="/"><a><i className="material-icons">assignment_ind</i> Home</a></Link>
       </nav>
     </header>
 
