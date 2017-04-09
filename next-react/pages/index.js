5833d21583a925909d009f03614e5810b07aecc4
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-08 20:22:23 -0500

    switch to pure css

--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 
 export default () =>
-  <h1>Hello World</h1>
+  (
+    <div>
+      <Head>
+        <title>EPL Score Board</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.1/build/pure-min.css" />
+      </Head>
+      <h1>Hello in a Material Way!!</h1>
+    </div>
+  )
