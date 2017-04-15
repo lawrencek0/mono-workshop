63fe8035b71169b52f1323ef489746ed90374153
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-14 21:43:06 -0500

    implement material-ui in project

--- js-projects-in-react/src/index.js
+++ js-projects-in-react/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import App from './App';
 import './index.css';
 
+injectTapEventPlugin();
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept();
+}
