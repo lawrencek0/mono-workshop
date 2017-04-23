6b20d7bccc132f47c7eb43b53f1e404a9cf88114
Lawrence Khadka <lawrence_2009@hotmail.com>
2017-04-23 01:43:18 -0500

    implement form and arrange posts

--- react-redux-projects/blog/src/App.js
+++ react-redux-projects/blog/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home  from './components/Home';
+import PostsNew from './components/Posts/New';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        ya i m the app bitch
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path="/posts/new" component={PostsNew}/>
+        </Switch>
       </div>
     );
   }
--- react-redux-projects/blog/src/index.js
+++ react-redux-projects/blog/src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import App from './App';
 import './index.css';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
-        <Route exact path="/" component={App}/>
+        <Route path="/" component={App}/>
     </Router>
   </Provider>,
   document.getElementById('root')
