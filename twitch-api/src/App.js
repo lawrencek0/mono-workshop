c4e5732e362251b721bf24fc0c6f2e2acfd7e7d6
LKhadka <lawrence_2009@hotmail.com>
2017-07-06 12:18:23 -0500

    display streams using proxy url to avoid cors

--- twitch-api/src/App.js
+++ twitch-api/src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { PROXY_URL, BASE_URL, TWITCH_STREAMERS } from './constants';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      streams: [],
+    };
+  };
+
+  componentDidMount() {
+    this.getUsers()
+  }
+
+  getUsers() {
+    TWITCH_STREAMERS.map(streamer => {
+      return fetch(`${PROXY_URL}/${BASE_URL}/streams/${streamer}`)
+        .then(res => res.json())
+        .then(json => this.setState({ streams: [...this.state.streams, json] }));
+    });
+  }
+
   render() {
     return (
       <div>
+        {this.state.streams.map((stream, index) => {
+          if (!stream.stream) {
+            return <div key={index}>Null</div>
+          }
+          return <div key={index}>{stream.stream.delay}</div>
+        })}
       </div>
     );
   }
