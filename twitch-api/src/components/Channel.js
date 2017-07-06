0dcf33599078d403786aec01be8511fa69ff7c48
LKhadka <lawrence_2009@hotmail.com>
2017-07-06 13:17:48 -0500

    add channel component

--- twitch-api/src/components/Channel.js
+++ twitch-api/src/components/Channel.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
-const Channel = ({ logo, })
+const Channel = ({ logo, name, status }) => {
+  return (
+    <div className="card">
+      <div className="logo">
+        <img src={logo} />
+      </div>
+      <div className="name">
+        <h3>{stream}</h3>
+      </div>
+      <div className="status">
+        {status}
+      </div>
+    </div>
+  )
+}
