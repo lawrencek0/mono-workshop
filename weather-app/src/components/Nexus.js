5a3932e1632df84b25ce8663af4a14f290b6f046
LKhadka <lawrence_2009@hotmail.com>
2017-07-08 15:46:51 -0500

    add nav, power and volume buttons

--- weather-app/src/components/Nexus.js
+++ weather-app/src/components/Nexus.js
@@ -9,7 +9,15 @@ class Nexus extends Component {
           <div className="camera"></div>
           <div className="speaker"></div>
         </header>
-        <div className="screen"></div>
+        <div className="screen">
+          <div className="power-button"></div>
+          <div className="volume-button"></div>
+          <div className="nav">
+            <div className="back"></div>
+            <div className="home"></div>  
+            <div className="overview"></div>
+          </div>  
+        </div> 
         <footer>
           <div className="speaker"></div>
         </footer>
