bc0da68d5f2f0ea92d151a881fe8cf00425b4deb
LKhadka <lawrence_2009@hotmail.com>
2017-07-08 11:08:21 -0500

    create component to create nexus mockup

---
+++ weather-app/src/components/Nexus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React';
+
+class Nexus extends Component {
+  render() {
+    return (
+      <div className="device">
+        
+      </div> 
+    )
+  }
+}
+
+export default Nexus;
