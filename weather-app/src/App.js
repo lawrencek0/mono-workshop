3801ffa596449cc955f5bdae5f7bd544388c9b90
LKhadka <lawrence_2009@hotmail.com>
2017-07-10 12:23:43 -0500

    implement geolocation

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { PROXY_URL, BASE_URL, PARAM, API_KEY } from './constants';
 import Nexus from './components/Nexus';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.requestLocation();
+  }
+  requestLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.useLocation);
+    }
+  }
+
+  useLocation(pos) {
+    fetch(`${PROXY_URL}/${BASE_URL}/${PARAM}/${API_KEY}/${pos.coords.latitude},${pos.coords.longitude}`).then(res => res.json()).then(json => console.log(json));
+  }
+
   render() {
     return (
       <div className="container">
