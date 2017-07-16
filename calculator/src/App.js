327f76a5178a844eb5238555ea1c0ddc22a51520
LKhadka <lawrence_2009@hotmail.com>
2017-07-16 12:33:15 -0500

    create buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0,
+      
+
+    }
+  }
   render() {
     return (
       <div className="container">
