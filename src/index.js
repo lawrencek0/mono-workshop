4f21ca24bbfb9670a366659a61a2ceaaee9b5e7a
LKhadka <lawrence_2009@hotmail.com>
2017-11-14 15:26:55 -0600

    add logging in to PET

--- src/index.js
+++ src/index.js
@@ -17,39 +17,79 @@ console.log(
   )
 );
 
-function getCreds(callback) {
-  const questions = [
-    {
-      name: 'email',
-      type: 'input',
-      message: 'Enter your PET e-mail address:',
-      validate(value) {
-        if (value.length) {
-          return true;
+async function loginToPET() {
+  const prefs = new Preferences('pet.tools.updater');
+  if (!prefs.email || !prefs.password) {
+    const questions = [
+      {
+        name: 'email',
+        type: 'input',
+        message: 'Enter your PET e-mail address:',
+        validate(value) {
+          if (value.length) {
+            return true;
+          }
+          return 'Please enter your e-mail address.';
         }
-        return 'Please enter your e-mail address.';
-      }
-    },
-    {
-      name: 'password',
-      type: 'password',
-      message: 'Enter your password:',
-      validate(value) {
-        if (value.length) {
-          return true;
+      },
+      {
+        name: 'password',
+        type: 'password',
+        message: 'Enter your password:',
+        validate(value) {
+          if (value.length) {
+            return true;
+          }
+          return 'Please enter your password.';
         }
-        return 'Please enter your password.';
       }
-    }
-  ];
+    ];
 
-  inquirer.prompt(questions).then(callback);
+    await inquirer.prompt(questions).then(({ email, password }) => {
+      prefs.email = email;
+      prefs.password = password;
+    });
+  }
+  tryToLogin(prefs);
 }
 
-function tryToLogin() {
-  const prefs = new Preferences('pet.tools.updater');
-
-  if (prefs.email && prefs.password) {
-    return;
+async function tryToLogin(prefs) {
+  const { email, password } = prefs;
+  const status = new Spinner('Authenticating you, please wait...');
+  status.start();
+  const nightmare = new Nightmare({ show: true });
+  try {
+    await nightmare
+      .goto('http://phageenzymetools.com/login')
+      .wait('input#inputEmail')
+      .insert('input#inputEmail', email)
+      .insert('input#inputPassword', password)
+      .click('input#inputPassword + button.btn')
+      .wait(1000)
+      .exists('span[style="color: red; "]')
+      .then(function(result) {
+        status.stop();
+        if (result) {
+          nightmare.end().then(() => {
+            console.log(
+              chalk.red(
+                `${chalk.bgBlackBright(
+                  'ERROR!'
+                )} Invalid Email or password!! Try Again!`
+              )
+            );
+            prefs.clear();
+            fetchCreds();
+          });
+        } else {
+          console.log(
+            chalk.green(`${chalk.bgBlackBright('SUCCESS!')} Logged In!`)
+          );
+        }
+      });
+  } catch (e) {
+    console.error(e);
   }
 }
+
+loginToPET();
