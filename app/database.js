8fd54d13446b95a6f8429d3f8004146e805206e8
LKhadka <lawrence_2009@hotmail.com>
2018-01-18 15:13:30 -0600

    create multiple tables from genera

--- app/database.js
+++ app/database.js
@@ -1,5 +1,6 @@
 import knex from 'knex';
 import 'sqlite3';
+import { GENERA } from './constants';
 
 const database = knex({
   client: 'sqlite3',
@@ -9,30 +10,32 @@ const database = knex({
   useNullAsDefault: true
 });
 
-/* eslint-disable promise/catch-or-return, promise/always-return */
-database.schema.hasTable('petPhages').then(exists => {
-  if (!exists) {
-    return database.schema.createTable('petPhages', t => {
-      t.primary('phage_name');
-      t.string('genus', 32);
-      t.string('cluster', 16);
-      t.string('subcluster', 8);
-    });
-  }
-});
+/* eslint-disable array-callback-return, promise/catch-or-return, promise/always-return */
+GENERA.map(genus => {
+  database.schema.hasTable(`${genus}PetPhages`).then(exists => {
+    if (!exists) {
+      return database.schema.createTable('petPhages', t => {
+        t.primary('phage_name');
+        t.string('genus', 32);
+        t.string('cluster', 16);
+        t.string('subcluster', 8);
+      });
+    }
+  });
 
-database.schema.hasTable('phagesDbPhages').then(exists => {
-  if (!exists) {
-    return database.schema.createTable('petPhages', t => {
-      t.primary('phage_name');
-      t.string('old_name');
-      t.string('genus', 32);
-      t.string('cluster', 16);
-      t.string('subcluster', 8);
-      t.string('end_type', 64);
-      t.string('fasta_file');
-    });
-  }
+  database.schema.hasTable(`${genus}PhagesDbPhages`).then(exists => {
+    if (!exists) {
+      return database.schema.createTable('petPhages', t => {
+        t.primary('phage_name');
+        t.string('old_name');
+        t.string('genus', 32);
+        t.string('cluster', 16);
+        t.string('subcluster', 8);
+        t.string('end_type', 64);
+        t.string('fasta_file');
+      });
+    }
+  });
 });
 /* eslint-enable */
 
