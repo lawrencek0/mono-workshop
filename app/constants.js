434b06aaa77c93cb849462fe4fc9b0a69843fcca
LKhadka <lawrence_2009@hotmail.com>
2018-01-19 10:14:40 -0600

    use values for genus to use for phages api

--- app/constants.js
+++ app/constants.js
@@ -1,20 +1,19 @@
 export const GENERA = [
-  'Mycobacteriophage',
-  'Rhodococcus',
-  'Arthrobacter',
-  'Streptomyces',
+  { name: 'Mycobacteriophage', value: 1 },
+  { name: 'Rhodococcus', value: 2 },
+  { name: 'Arthrobacter', value: 3 },
+  { name: 'Streptomyces', value: 4 },
   // TODO: Run Nightmare for scraping Bacillus
-  // 'Bacillus',
-  'Gordonia',
-  'Corynebacterium',
-  'Propionibacterium',
-  'Actinoplanes',
-  'Tetrasphaera',
-  'Tsukamurella',
-  'Microbacterium',
-  // 'Dietzia',
-  'Rothia',
-  'Brevibacterium'
+  // { name: 'Bacillus', value: 5 },
+  { name: 'Gordonia', value: 6 },
+  { name: 'Corynebacterium', value: 7 },
+  { name: 'Propionibacterium', value: 8 },
+  { name: 'Actinoplanes', value: 9 },
+  { name: 'Tetrasphaera', value: 10 },
+  { name: 'Tsukamurella', value: 11 },
+  { name: 'Microbacterium', value: 12 }, // { name: 'Dietzia', value: 13 },
+  { name: 'Rothia', value: 14 },
+  { name: 'Brevibacterium', value: 15 }
 ];
 
 export const PET_URL = 'http://phageenzymetools.com/login';
