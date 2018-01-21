365a8013b2436bf06bbfdff4caab27f04feca4d6
LKhadka <lawrence_2009@hotmail.com>
2018-01-21 10:11:10 -0600

    experiment with chromeless

---
+++ app/lib/Scraper.js
@@ -0,0 +1,30 @@
+import { Chromeless } from 'chromeless';
+import { PET_URL } from '../constants';
+
+class Scraper {
+  constructor() {
+    this.chromeless = new Chromeless();
+  }
+
+  async startChromeless() {
+    await this.chromeless.goto(PET_URL);
+  }
+
+  async loginToPet(email, password) {
+    try {
+      await this.startChromeless();
+      const res = await this.chromeless
+        .wait('input#inputEmail')
+        .type(email, 'input#inputEmail')
+        .type(password, 'input#inputPassword')
+        .click('input#inputPassword + button.btn')
+        .wait(5000)
+        .exists('span[style="color: red; "]');
+      return !res;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export default new Scraper();
