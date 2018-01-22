b3d5d4aaada85c1dfaae7908defb398889f46ea0
LKhadka <lawrence_2009@hotmail.com>
2018-01-21 20:36:57 -0600

    add method to open selected genus

--- app/lib/Scraper.js
+++ app/lib/Scraper.js
@@ -3,7 +3,7 @@ import { PET_URL } from '../constants';
 
 class Scraper {
   constructor() {
-    this.chromeless = new Chromeless();
+    this.chromeless = new Chromeless({ scrollBeforeClick: true });
   }
 
   async startChromeless() {
@@ -22,6 +22,31 @@ class Scraper {
         .exists('span[style="color: red; "]');
 
       return !res;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async openGenus(genus) {
+    try {
+      await this.chromeless
+        .wait('ul.nav-sidebar')
+        .click('a[href="known_phage_visualization"]')
+        .wait('ul.tabs')
+        .type(genus, 'input[placeholder="Search Genera"]')
+        .click(`li[id$="-${genus}"]`)
+        .click('input[placeholder="Search Enzymes"]')
+        .wait('li[id$="-AanI"]')
+        .click('li[id$="-AanI"]')
+        .click('button#submit')
+        .wait('table#cutTable')
+        .click('#cutTable_length select[name="cutTable_length"]')
+        .evaluate(() => {
+          document.querySelector('#cutTable_length select[name="cutTable_length"] option[value="100"]').selected = 100;
+          document
+            .querySelector('#cutTable_length select[name="cutTable_length"]')
+            .dispatchEvent(new Event('change'));
+        });
     } catch (e) {
       console.error(e);
     }
