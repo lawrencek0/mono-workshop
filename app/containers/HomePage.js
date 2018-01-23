f124b2017cd060de44f1b56d00b708fb6f6e146f
LKhadka <lawrence_2009@hotmail.com>
2018-01-23 15:34:25 -0600

    add and update methods to update and save phages to db from api

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -6,14 +6,13 @@ import Home from '../components/Home';
 import database from '../database';
 import scraper from '../lib/Scraper';
 import { GENERA } from '../constants';
-import { formatPhageDbPhages } from '../utils/PhageFormatter';
-import { savePhageToDb, getPetCreds, savePetCreds } from '../utils/Misc';
+import {
+  savePhageToDb,
+  getPetCreds,
+  getPhagesFromPhagesDbApi
+} from '../utils/Misc';
 
 class HomePage extends Component {
-  componentDidMount() {
-    this.loginToPet();
-  }
-
   async loginToPet() {
     const [creds] = await getPetCreds();
     if (!creds) {
@@ -31,9 +30,17 @@ class HomePage extends Component {
     try {
       await scraper.openGenus(genus);
       const phages = await scraper.scrapePhagesFromPet(genus);
-      await Promise.all(phages.map(async phage => {
-        savePhageToDb(`${phage.genus}PetPhages`, phage);
-      }));
+      await Promise.all(phages.map(phage => savePhageToDb(`${phage.genus}PetPhages`, phage)));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  updatePhagesDbPhages = async genus => {
+    try {
+      const { value } = GENERA.find(({ name }) => name === genus);
+      const phages = await getPhagesFromPhagesDbApi(value);
+      await Promise.all(phages.map(phage => savePhageToDb(`${genus}PhagesDb`, phage)));
     } catch (e) {
       console.error(e);
     }
@@ -49,6 +56,14 @@ class HomePage extends Component {
     }
   };
 
+  updateAllPhagesDbPhages = async () => {
+    try {
+      await Promise.all(GENERA.map(({ name }) => this.updatePhagesDbPhages(name)));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   fetchPhagesDbPhages = async genus => database(`${genus}PhagesDb`).select();
 
   fetchAllPhagesDbPhages = async () => {
@@ -59,42 +74,6 @@ class HomePage extends Component {
     }
   };
 
-  updateAllPhagesDbPhages = async () => {
-    try {
-      const phages = [].concat(...(await Promise.all(GENERA.map(({ value }) => this.fetchPhagesFromPhagesDb(value)))));
-      await Promise.all(phages.map(async phage => {
-        const genus =
-            phage.genus === 'Mycobacterium' ? 'Mycobacteriophage' : phage.genus;
-        const phageName = await database(`${genus}PhagesDb`)
-          .where({
-            phage_name: phage.phage_name
-          })
-          .select('phage_name');
-        if (phageName.length === 0) {
-          database(`${genus}PhagesDb`)
-            .insert(phage)
-            .catch(console.error);
-        }
-      }));
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  fetchPhagesFromPhagesDb = async (pk, pageNum = 1, phages = []) => {
-    try {
-      const res = await fetch(`http://phagesdb.org/api/host_genera/${pk}/phagelist/?page=${pageNum}`);
-      const { next, results } = await res.json();
-      const allPhages = phages.concat(...formatPhageDbPhages(results));
-      if (next) {
-        const num = pageNum + 1;
-        return this.fetchPhagesFromPhagesDb(pk, num, allPhages);
-      }
-      return allPhages;
-    } catch (e) {
-      console.error(e);
-    }
-  };
   render() {
     return <Home />;
   }
--- app/utils/Misc.js
+++ app/utils/Misc.js
@@ -1,5 +1,7 @@
 import { remote } from 'electron';
 import database from '../database';
+import { formatPhageDbPhages } from '../utils/PhageFormatter';
+import { GENERA } from '../constants';
 
 const keytar = remote.require('keytar');
 
@@ -12,14 +14,25 @@ export async function savePetCreds(email, password) {
 }
 
 export async function savePhageToDb(tableName, phage) {
-  const phageName = await database(`${phage.genus}PetPhages`)
+  const phageName = await database(tableName)
     .where({ phage_name: phage.phage_name })
     .select('phage_name');
 
   if (phageName.length === 0) {
-    database(`${phage.genus}PetPhages`)
+    database(tableName)
       .insert(phage)
       .then(console.log)
       .catch(console.error);
   }
+}
+
+export async function getPhagesFromPhagesDbApi(pk, pageNum = 1, phages = []) {
+  const res = await fetch(`http://phagesdb.org/api/host_genera/${pk}/phagelist/?page=${pageNum}`);
+  const { next, results } = await res.json();
+  const allPhages = phages.concat(...formatPhageDbPhages(results));
+  if (next) {
+    const num = pageNum + 1;
+    return getPhagesFromPhagesDbApi(pk, num, allPhages);
+  }
+  return allPhages;
 }
