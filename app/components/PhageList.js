db6e6a45099ea24d6c95edaf414c39913eb14fdf
LKhadka <lawrence_2009@hotmail.com>
2018-01-26 22:35:37 -0600

    split table to genera

--- app/components/PhageList.js
+++ app/components/PhageList.js
@@ -1,28 +1,53 @@
 import React, { Fragment } from 'react';
-import { Header, Table } from 'semantic-ui-react';
+import { Header, Table, Tab } from 'semantic-ui-react';
 import Phage from './Phage';
+import { GENERA } from '../constants';
 
-const PhageList = ({ heading, phages, viewPhage }) => (
-  <Fragment>
-    <Header as="h2" textAlign="center">
-      {heading} ({phages.length})
-    </Header>
-    <Table basic="very" celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Phage</Table.HeaderCell>
-          <Table.HeaderCell>Genus</Table.HeaderCell>
-          <Table.HeaderCell>Cluster</Table.HeaderCell>
-          <Table.HeaderCell>Subcluster</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {phages.map(phage => (
-          <Phage key={phage.phageName} phage={phage} viewPhage={viewPhage} />
-        ))}
-      </Table.Body>
-    </Table>
-  </Fragment>
-);
+const PhageList = ({ heading, phages, viewPhage }) => {
+  const panes = () =>
+    GENERA.map(({ name }) => {
+      const genusPhages = phages.filter(phage => phage.genus === name);
+      if (genusPhages.length !== 0) {
+        return {
+          menuItem: name,
+          pane: (
+            <Tab.Pane attached={false} key={heading + name}>
+              <Table basic="very" celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Phage</Table.HeaderCell>
+                    <Table.HeaderCell>Genus</Table.HeaderCell>
+                    <Table.HeaderCell>Cluster</Table.HeaderCell>
+                    <Table.HeaderCell>Subcluster</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {genusPhages.map(phage => (
+                    <Phage
+                      key={phage.phageName}
+                      phage={phage}
+                      viewPhage={viewPhage}
+                    />
+                  ))}
+                </Table.Body>
+              </Table>
+            </Tab.Pane>
+          )
+        };
+      }
+    }).filter(genus => Boolean(genus));
+  return (
+    <Fragment>
+      <Header as="h2" textAlign="center">
+        New phages in {heading} ({phages.length})
+      </Header>
+      <Tab
+        renderActiveOnly={false}
+        menu={{ secondary: true, pointing: true }}
+        panes={panes()}
+      />
+    </Fragment>
+  );
+};
 
 export default PhageList;
