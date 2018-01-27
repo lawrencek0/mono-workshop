77a334131d86f989f8a994445419fd8d0522d8c9
LKhadka <lawrence_2009@hotmail.com>
2018-01-26 22:42:40 -0600

    add margin top

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -138,7 +138,7 @@ class HomePage extends Component {
     }
 
     return (
-      <Container>
+      <Container style={{ marginTop: '3rem' }}>
         <Tab
           renderActiveOnly={false}
           menu={{ secondary: true, pointing: true, vertical: true }}
