fec82e6ec5dc0d15d2bf60a71f189b7c8bfb9a02
LKhadka <lawrence_2009@hotmail.com>
2018-01-28 16:28:57 -0600

    add function for logging in to pet

--- app/lib/Puppeteer.js
+++ app/lib/Puppeteer.js
@@ -5,6 +5,34 @@ let browser = null;
 let page = null;
 
 export const startScraper = async () => {
-  browser = await puppeteer.launch({ headless: false });
+  browser = await puppeteer.launch({ headless: false, slowMo: 250 });
   page = await browser.newPage();
+};
+
+// tries to log in the user with given creds
+// returns null when successfully signed in
+// returns a selector when error happens
+export const loginToPet = async (email, password) => {
+  const USERNAME_SELECTOR = '#inputEmail';
+  const PASSWORD_SELECTOR = '#inputPassword';
+  const SIGN_IN_BTN_SELECTOR = '#inputPassword + button.btn';
+  const ERROR_MSG_SELECTOR = 'span[style="color: red; "]';
+
+  try {
+    await page.goto(PET_URL);
+
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(email);
+
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(password);
+
+    await page.click(SIGN_IN_BTN_SELECTOR);
+
+    await page.waitFor(2000);
+
+    return page.$(ERROR_MSG_SELECTOR);
+  } catch (e) {
+    console.error(e);
+  }
 };
