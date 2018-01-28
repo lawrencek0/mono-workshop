e4686605067efdc6451eb982844da3837b6210ec
LKhadka <lawrence_2009@hotmail.com>
2018-01-28 17:34:38 -0600

    add function for opening genus in pet

--- app/lib/Puppeteer.js
+++ app/lib/Puppeteer.js
@@ -37,6 +37,38 @@ export const loginToPet = async (email, password) => {
   }
 };
 
+export const openGenus = async genus => {
+  const PHAGE_SIDEBAR_SELECTOR = 'a[href="known_phage_visualization"]';
+  const GENERA_SEARCH_SELECTOR = 'input[placeholder="Search Genera"]';
+  const GENUS_DROPDOWN_SELECTOR = `li[id$="-${genus}"]`;
+  const ENZYMES_SEARCH_SELECTOR = 'input[placeholder="Search Enzymes"]';
+  const ENZYME_DROPDOWN_SELECTOR = 'li[id$="-AanI"]';
+  const GENERA_SEARCH_BUTTON = '#submit';
+  const CUT_TABLE_LENGTH_SELECT_SELECTOR = 'select[name="cutTable_length"]';
+  const CUT_TABLE_LENGTH_VALUE = '100';
+
+  try {
+    await page.click(PHAGE_SIDEBAR_SELECTOR);
+
+    await page.waitForSelector(GENERA_SEARCH_SELECTOR);
+
+    await page.click(GENERA_SEARCH_SELECTOR);
+    await page.keyboard.type(genus);
+    await page.click(GENUS_DROPDOWN_SELECTOR);
+
+    await page.click(ENZYMES_SEARCH_SELECTOR);
+    await page.click(ENZYME_DROPDOWN_SELECTOR);
+
+    await page.click(GENERA_SEARCH_BUTTON);
+
+    await page.waitForSelector(CUT_TABLE_LENGTH_SELECT_SELECTOR);
+
+    await page.select(CUT_TABLE_LENGTH_SELECT_SELECTOR, CUT_TABLE_LENGTH_VALUE);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export const closeScraper = async () => {
   await browser.close();
 };
