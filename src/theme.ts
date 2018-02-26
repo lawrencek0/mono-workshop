21cd13ef3ec7a546bf2648f62c1f9eb2ee7f11cb
LKhadka <l.khadka@outlook.com>
2018-02-26 16:27:42 -0600

    create interfaces and functions for styled components

---
+++ src/theme.ts
@@ -0,0 +1,30 @@
+import * as styledComponents from 'styled-components';
+
+export interface ThemeInterface {
+  primaryColor: string;
+  secondaryColor: string;
+}
+
+const {
+  default: styled,
+  css,
+  injectGlobal,
+  keyframes,
+  ThemeProvider
+} = styledComponents as styledComponents.ThemedStyledComponentsModule<
+  ThemeInterface
+>;
+
+export const dayTheme = {
+  primaryColor: '#FFE519',
+  secondaryColor: '#6F4C30'
+};
+
+export const nightTheme = {
+  primaryColor: '#2F3540',
+  secondaryColor: '#778E93'
+};
+
+export { css, injectGlobal, keyframes, ThemeProvider };
+
+export default styled;
