e5f333eb7ea06b22fd17f023c0f38437f6e7d5b8
LKhadka <l.khadka@outlook.com>
2018-02-27 13:20:39 -0600

    create stylish header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
+import styled, { ThemeInterface } from '../theme';
 
-const Header = () => (
-  <header>
-    <h1>
-      Sweeet Pumpkins<sup>(TX)</sup>
-    </h1>
+interface HeaderProps {
+  theme?: ThemeInterface;
+  className?: string;
+}
+
+// @TODO: after adding React-Router make this button navigate to home
+const Header: React.StatelessComponent<HeaderProps> = props => (
+  <header className={props.className}>
+    <h1>Sweeet Pumpkins (TX)</h1>
   </header>
 );
 
-export default Header;
+const StyledHeader = styled(Header)`
+  background-color: ${props => props.theme.primaryColor};
+  font-family: 'Amatic SC', cursive;
+  font-size: 1.75rem;
+  height: 5.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default StyledHeader;
