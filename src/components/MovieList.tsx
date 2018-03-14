a14c17daf24328f5e42e6713fe8872eb0d89269a
LKhadka <l.khadka@outlook.com>
2018-03-14 16:40:36 -0500

    replace internal state to use redux's movies state

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,62 +1,20 @@
 import * as React from 'react';
 import styled from '../theme';
-import Movie, { MovieProps } from './Movie';
-
-interface MovieListState {
-  movies: MovieProps[];
-}
+import MovieItem from './MovieItem';
+import { Movie } from '../redux/movies';
 
 interface MovieListProps {
+  movies: Movie[];
   className?: string;
 }
 
-class MovieList extends React.Component<MovieListProps, MovieListState> {
-  state: MovieListState = { movies: [] };
-
-  componentDidMount() {
-    this.fetchMovies();
-  }
-
-  fetchMovies = async () => {
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${
-          process.env.REACT_APP_TMDB_API_KEY
-        }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
-      );
-      const { results } = await res.json();
-      const movies: MovieProps[] = results.map(
-        ({
-          vote_count,
-          id,
-          genre_ids,
-          poster_path,
-          title,
-          vote_average,
-          release_date
-        }: MovieProps) => ({
-          vote_count,
-          id,
-          genre_ids,
-          poster_path,
-          title,
-          vote_average,
-          release_date
-        })
-      );
-      this.setState({ movies });
-    } catch (e) {
-      throw Error('Error fetching results!\n' + e);
-    }
-  };
-
+class MovieList extends React.Component<MovieListProps> {
   render() {
-    if (!this.state.movies) {
-      return <span>Loading</span>;
-    }
     return (
       <ul className={this.props.className}>
-        {this.state.movies.map(movie => <Movie key={movie.id} {...movie} />)}
+        {this.props.movies.map(movie => (
+          <MovieItem key={movie.id} {...movie} />
+        ))}
       </ul>
     );
   }
