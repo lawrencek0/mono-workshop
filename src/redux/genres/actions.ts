c3d50308976c4103be628cfeda0280a9df8d96e7
LKhadka <l.khadka@outlook.com>
2018-03-18 17:42:32 -0500

    remove redundant return type

--- src/redux/genres/actions.ts
+++ src/redux/genres/actions.ts
@@ -2,9 +2,7 @@ import { createAction } from 'typesafe-actions';
 import { FETCH_GENRES, FETCH_GENRES_FULFILLED } from './types';
 
 export const genresActions = {
-  fetchGenres: createAction(FETCH_GENRES, () => ({
-    type: FETCH_GENRES
-  })),
+  fetchGenres: createAction(FETCH_GENRES),
 
   fetchGenresFulfilled: createAction(FETCH_GENRES_FULFILLED, payload => ({
     type: FETCH_GENRES_FULFILLED,
--- src/redux/movies/actions.ts
+++ src/redux/movies/actions.ts
@@ -2,9 +2,7 @@ import { createAction } from 'typesafe-actions';
 import { FETCH_MOVIES, FETCH_MOVIES_FULFILLED } from './types';
 
 export const moviesActions = {
-  fetchMovies: createAction(FETCH_MOVIES, () => ({
-    type: FETCH_MOVIES
-  })),
+  fetchMovies: createAction(FETCH_MOVIES),
 
   fetchMoviesFullFilled: createAction(FETCH_MOVIES_FULFILLED, payload => ({
     type: FETCH_MOVIES_FULFILLED,
