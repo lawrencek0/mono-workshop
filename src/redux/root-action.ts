5a532a7a1392a2008994aef010d5438dced0dc18
LKhadka <l.khadka@outlook.com>
2018-03-18 20:12:50 -0500

    remove usage of navigation state

--- src/redux/root-action.ts
+++ src/redux/root-action.ts
@@ -1,12 +1,10 @@
 import { $call } from 'utility-types';
 import { moviesActions } from './movies';
 import { genresActions } from './genres';
-import { navigationActions } from './navigation';
 
 const returnsOfActions = [
   ...Object.values(moviesActions),
-  ...Object.values(genresActions),
-  ...Object.values(navigationActions)
+  ...Object.values(genresActions)
 ].map($call);
 
 type AppAction = typeof returnsOfActions[number];
