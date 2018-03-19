c2db62dd8de449d9b2df7a1e14659a1044799cf6
LKhadka <l.khadka@outlook.com>
2018-03-18 20:01:35 -0500

    move ephemeral form state back to react component

--- src/redux/navigation/types.ts
+++
@@ -1,4 +0,0 @@
-export const UPDATE_SELECTED_GENRE = '@@navigation/UPDATE_SELECTED_GENRE';
-export const UPDATE_YEAR_SLIDER = '@@navigation/UPDATE_YEAR_SLIDER';
-export const UPDATE_RATING_SLIDER = '@@navigation/UPDATE_RATING_SLIDER';
-export const UPDATE_RUNTIME_SLIDER = '@@navigation/UPDATE_RUNTIME_SLIDER';
