179f935ab98fbed90a027899d3d0a826b0461267
LKhadka <l.khadka@outlook.com>
2018-03-19 19:57:31 -0500

    create constants file for storing url parts

---
+++ src/redux/movies/constants.ts
@@ -0,0 +1,16 @@
+export const PATH_BASE = 'https://api.themoviedb.org/3';
+export const PATH_DISCOVER = '/discover';
+export const PATH_MOVIE = '/movie';
+export const PARAM_API_KEY = 'api_key=';
+export const PARAM_LANGUAGE = 'language=';
+export const PARAM_SORT_BY = 'sort_by=';
+export const PARAM_WITH_GENRES = 'with_genres';
+export const PARAM_PRIMARY_RELEASE_DATE = 'primary_release_date=';
+export const PARAM_VOTE_AVERAGE = 'vote_average=';
+export const PARAM_WITH_RUNTIME = 'with_runtime=';
+export const PARAM_PAGE = 'page=';
+export const MODIFIER_GREATER_THAN = 'gte';
+export const MODIFIER_LESS_THAN = 'lte';
+export const DEFAULT_LANGUAGE = 'en-US';
+export const DEFAULT_SORT_BY = 'popularity.desc';
+export const DEFAULT_PAGE = '1';
