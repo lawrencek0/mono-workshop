57e7d6f7f11155c0fe768ac981d0fe2d910ab72e
LKhadka <l.khadka@outlook.com>
2018-03-21 21:43:50 -0500

    make constants declaration consistent by removing = operator

--- src/redux/movies/constants.ts
+++ src/redux/movies/constants.ts
@@ -1,10 +1,10 @@
 export const PATH_BASE = 'https://api.themoviedb.org/3';
 export const PATH_DISCOVER = '/discover';
 export const PATH_MOVIE = '/movie';
-export const PARAM_API_KEY = 'api_key=';
-export const PARAM_LANGUAGE = 'language=';
-export const PARAM_SORT_BY = 'sort_by=';
-export const PARAM_WITH_GENRES = 'with_genres=';
+export const PARAM_API_KEY = 'api_key';
+export const PARAM_LANGUAGE = 'language';
+export const PARAM_SORT_BY = 'sort_by';
+export const PARAM_WITH_GENRES = 'with_genres';
 export const PARAM_PRIMARY_RELEASE_DATE = 'primary_release_date';
 export const PARAM_VOTE_AVERAGE = 'vote_average';
 export const PARAM_WITH_RUNTIME = 'with_runtime';
