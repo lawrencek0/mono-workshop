d3121b3a28dc91a9b963485260926984fd92462c
LKhadka <l.khadka@outlook.com>
2018-03-21 21:56:47 -0500

    update action creator return type

--- src/redux/movies/actions.ts
+++ src/redux/movies/actions.ts
@@ -1,8 +1,19 @@
 import { createAction } from 'typesafe-actions';
 import { FETCH_MOVIES, FETCH_MOVIES_FULFILLED } from './types';
+import { RangeData } from '../../components/SideBar';
+
+export interface TmdbApiParams {
+  genreId: number;
+  year: RangeData;
+  rating: RangeData;
+  runtime: RangeData;
+}
 
 export const moviesActions = {
-  fetchMovies: createAction(FETCH_MOVIES),
+  fetchMovies: createAction(FETCH_MOVIES, (payload: TmdbApiParams) => ({
+    type: FETCH_MOVIES,
+    payload
+  })),
 
   fetchMoviesFullFilled: createAction(FETCH_MOVIES_FULFILLED, payload => ({
     type: FETCH_MOVIES_FULFILLED,
