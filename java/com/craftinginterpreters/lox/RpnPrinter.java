dcf5c5204430b01f7423719a2db77a7624887e9c
lawrencek0 <l.khadka@outlook.com>
2019-01-15 16:40:59 -0600

    add printer using reverse polish notation

---
+++ java/com/craftinginterpreters/lox/RpnPrinter.java
@@ -0,0 +1,66 @@
+package com.craftinginterpreters.lox;
+
+class RpnPrinter implements Expr.Visitor<String> {
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  private String parenthesize(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("(");
+    for (Expr expr: exprs) {
+      builder.append(expr.accept(this));
+      builder.append(" ");
+    }
+    builder.append(name).append(")");
+
+    return builder.toString();
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return expr.expression.accept(this);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null) {
+      return "nil";
+    }
+
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+      new Expr.Grouping(
+        new Expr.Binary(
+          new Expr.Literal(1),
+          new Token(TokenType.PLUS, "+", null, 1),
+          new Expr.Literal(2)
+        )
+      ),
+      new Token(TokenType.PLUS, "+", null, 1),
+      new Expr.Grouping(
+        new Expr.Binary(
+          new Expr.Literal(4),
+          new Token(TokenType.MINUS, "-", null, 1),
+          new Expr.Literal(3)
+        )
+      )
+    );
+
+    System.out.println(new RpnPrinter().print(expression));
+  }
+}
