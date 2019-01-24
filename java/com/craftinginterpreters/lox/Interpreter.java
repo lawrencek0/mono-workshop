5a32aa6863de28f94c8d4ee4727881b5053e2258
lawrencek0 <l.khadka@outlook.com>
2019-01-23 22:29:57 -0600

    add resolver for handling resolutions and bindings

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,13 +1,16 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
   
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -70,6 +73,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     } finally {
       this.environment = previous;
     }
+  }
+  
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
   }
   
   @Override
@@ -214,7 +221,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   
   @Override
   public Object visitVarExpr(Expr.Var expr) { 
-    return environment.get(expr.name);
+    return lookupVariable(expr.name, expr);
+  }
+  
+  private Object lookupVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
   }
   
   @Override
