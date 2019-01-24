d5718a05ce52284d8f946c5fa563c4dbbd5fde37
lawrencek0 <l.khadka@outlook.com>
2019-01-23 23:00:53 -0600

    add code for declaraing classes

---
+++ java/com/craftinginterpreters/lox/LoxClass.java
@@ -0,0 +1,31 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class LoxClass implements LoxCallable {
+  final String name;
+  
+  LoxClass(String name) {
+    this.name = name;
+  }
+  
+  @Override
+  public String toString() {
+    return name;
+  }
+  
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    return instance;
+  }
+  
+  @Override
+  public int arity() {
+    return 0;
+  }
+  
+}
