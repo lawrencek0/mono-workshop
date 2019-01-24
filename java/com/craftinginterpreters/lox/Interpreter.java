be15a50da3e485983d1783a5de89d48d4d831564
lawrencek0 <l.khadka@outlook.com>
2019-01-24 16:56:36 -0600

    add properties to class instances

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -88,7 +88,12 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
     environment.define(stmt.name.lexeme, null);
-    LoxClass klass = new LoxClass(stmt.name.lexeme);
+    Map<String, LoxFunction> methods = new HashMap<>();
+    for (Stmt.Function method: stmt.methods) {
+      LoxFunction function = new LoxFunction(method, environment);
+      methods.put(method.name.lexeme, function);
+    }
+    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
     environment.assign(stmt.name, klass);
     return null;
   }
@@ -208,6 +213,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
   
   @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+    
+    if(!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name, "Only instances have fields.");
+    }
+    
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
+  }
+  
+  @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
   }
@@ -317,5 +335,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     
     return function.call(this, arguments);
+  }
+  
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+    
+    throw new RuntimeError(expr.name, "Only instances have properties.");
   }
 }
--- java/com/craftinginterpreters/lox/Parser.java
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -302,6 +302,9 @@ class Parser {
       if (expr instanceof Expr.Var) {
         Token name = ((Expr.Var)expr).name;
         return new Expr.Assign(name, value);
+      } else if (expr instanceof Expr.Get) {
+        Expr.Get get = (Expr.Get)expr;
+        return new Expr.Set(get.object, get.name, value);
       }
       
       error(equals, "Invalid assignment target.");
@@ -444,6 +447,9 @@ class Parser {
     while(true) {
       if (match(LEFT_PAREN)) {
         expr = finishCall(expr);
+      } else if (match(DOT)) {
+        Token name = consume(IDENTIFIER, "Expect property name after '.'");
+        expr = new Expr.Get(expr, name);
       } else {
         break;
       }
