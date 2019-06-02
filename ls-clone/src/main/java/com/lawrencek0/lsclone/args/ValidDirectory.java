ffe680a5190812656b5ef428f345934afb7302dd
lawrencek0 <l.khadka@outlook.com>
2019-06-01 22:08:00 -0500

    make class public for tests and fix logic

--- ls-clone/src/main/java/com/lawrencek0/lsclone/args/ValidDirectory.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/args/ValidDirectory.java
@@ -7,11 +7,11 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-class ValidDirectory implements IParameterValidator {
+public class ValidDirectory implements IParameterValidator {
     @Override
     public void validate(String name, String value) throws ParameterException {
         Path file = new File(value).toPath();
-        if (!Files.isDirectory(file) || !Files.isRegularFile(file)) {
+        if (!(Files.isDirectory(file) || Files.isRegularFile(file))) {
             throw new ParameterException("cannot access '" + value + "': No such file or directory");
         }
     }
