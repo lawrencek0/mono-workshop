8a2f897d46cbb3dde652aae54816c94d834a86c7
lawrencek0 <l.khadka@outlook.com>
2019-06-02 15:49:30 -0500

    make class public

--- ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
@@ -5,7 +5,7 @@ import com.beust.jcommander.Parameter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-class Args {
+public class Args {
     @Parameter(description = "Directory", validateWith = ValidPath.class, converter = PathConverter.class)
-    Path path = Paths.get(".");
+    public Path path = Paths.get(".");
 }
