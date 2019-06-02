13e98d536071f51bf15416981470c6753d77d513
lawrencek0 <l.khadka@outlook.com>
2019-06-01 22:52:14 -0500

    use Paths to get the path

--- ls-clone/src/main/java/com/lawrencek0/lsclone/args/ValidPath.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/args/ValidPath.java
@@ -3,14 +3,14 @@ package com.lawrencek0.lsclone.args;
 import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.ParameterException;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ValidPath implements IParameterValidator {
     @Override
     public void validate(String name, String value) throws ParameterException {
-        Path file = new File(value).toPath();
+        Path file = Paths.get(value);
         if (!(Files.isDirectory(file) || Files.isRegularFile(file))) {
             throw new ParameterException("cannot access '" + value + "': No such file or path");
         }
