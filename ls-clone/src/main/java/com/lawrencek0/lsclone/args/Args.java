456d4bd778cc14f7e9573c49a2b9be1e5f9d682b
lawrencek0 <l.khadka@outlook.com>
2019-06-02 19:07:35 -0500

    store values in ArrayList for JCommander to work

--- ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
@@ -4,8 +4,11 @@ import com.beust.jcommander.Parameter;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Args {
     @Parameter(description = "Directory", validateWith = ValidPath.class, converter = PathConverter.class)
-    public Path path = Paths.get(".");
+    public List<Path> paths = new ArrayList<>(Arrays.asList(Paths.get(".")));
 }
