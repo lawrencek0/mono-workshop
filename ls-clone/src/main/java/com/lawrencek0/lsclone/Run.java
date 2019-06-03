e40d70e521cbcf3fb30488b8818fc399799893b7
lawrencek0 <l.khadka@outlook.com>
2019-06-02 19:13:05 -0500

    refactor to handle multiple paths

--- ls-clone/src/main/java/com/lawrencek0/lsclone/Run.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/Run.java
@@ -3,7 +3,10 @@ package com.lawrencek0.lsclone;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.AbstractMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,15 +15,27 @@ import java.util.stream.Stream;
  */
 public class Run {
     /**
+     * Executes the passed in arguments
+     *
+     * @return The list of all entries in the given paths
+     */
+    public static Map<String, Set<String>> executeArgs(List<Path> paths) {
+        return paths.stream().map(p -> new AbstractMap.SimpleEntry<>(p.toString(), Run.fetchEntries(p)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    /**
      * Gets all the entries in the given path
      *
-     * @param path The path to get the entries from
-     * @return The list of all entries in the given path
-     * @throws IOException
+     * @param path The paths to get the entries from
+     * @return The list of all entries in the given paths
      */
-    public static List<String> executeArgs(Path path) throws IOException {
+    private static Set<String> fetchEntries(Path path) {
         try (Stream<Path> paths = Files.list(path)) {
-            return paths.map(p -> p.getFileName().toString()).collect(Collectors.toList());
+            return paths.map(p -> p.getFileName().toString()).collect(Collectors.toSet());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return Set.of(path.toString());
     }
 }
