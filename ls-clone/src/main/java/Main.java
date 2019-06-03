146a55e92ce2ed4e457f5dec192f892c1e7008a0
lawrencek0 <l.khadka@outlook.com>
2019-06-02 19:17:47 -0500

    refactor example to use multiple paths

--- ls-clone/src/main/java/Main.java
+++ ls-clone/src/main/java/Main.java
@@ -3,14 +3,16 @@ import com.beust.jcommander.ParameterException;
 import com.lawrencek0.lsclone.Run;
 import com.lawrencek0.lsclone.args.Args;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String... argv) throws IOException {
+    public static void main(String... argv) {
         Args args = new Args();
         try {
             JCommander.newBuilder().addObject(args).build().parse(argv);
-            Run.executeArgs(args.path).forEach(entry -> System.out.print(entry + "  "));
+            Run.executeArgs(args.paths).forEach((key, value) -> {
+                System.out.println(key + ":");
+                value.forEach(e -> System.out.print(e + "  "));
+                System.out.println();
+            });
         } catch (ParameterException e) {
             System.out.println(e.getMessage());
         }
