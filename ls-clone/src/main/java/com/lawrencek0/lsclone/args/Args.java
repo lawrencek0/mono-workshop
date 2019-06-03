595e3b46f8dc4d6d9812de27e2070855bfeb334d
lawrencek0 <l.khadka@outlook.com>
2019-06-02 19:08:30 -0500

    use singletonlist instead

--- ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
+++ ls-clone/src/main/java/com/lawrencek0/lsclone/args/Args.java
@@ -5,10 +5,10 @@ import com.beust.jcommander.Parameter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Args {
     @Parameter(description = "Directory", validateWith = ValidPath.class, converter = PathConverter.class)
-    public List<Path> paths = new ArrayList<>(Arrays.asList(Paths.get(".")));
+    public List<Path> paths = new ArrayList<>(Collections.singletonList(Paths.get(".")));
 }
