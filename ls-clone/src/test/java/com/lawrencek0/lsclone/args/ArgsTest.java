7e2a4418a1951f2366d26524e37e56453ee8b509
lawrencek0 <l.khadka@outlook.com>
2019-06-02 19:06:21 -0500

    static import test files

--- ls-clone/src/test/java/com/lawrencek0/lsclone/args/ArgsTest.java
+++ ls-clone/src/test/java/com/lawrencek0/lsclone/args/ArgsTest.java
@@ -13,32 +13,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class ArgsTest {
     @Test
     void testDefaultParameters() {
         Args args = new Args();
         JCommander.newBuilder().addObject(args).build().parse();
-        Assertions.assertEquals(List.of(Paths.get(".")), args.paths);
+        assertEquals(List.of(Paths.get(".")), args.paths);
     }
 
     @Test
     @DisplayName("Tests when single Path is passed in")
     void testSinglePaths(@TempDir Path tempDir) {
-        Assertions.assertTrue(Files.isDirectory(tempDir));
+        assertTrue(Files.isDirectory(tempDir));
         Args args = new Args();
         JCommander.newBuilder().addObject(args).build().parse(tempDir.toString());
-        Assertions.assertTrue(args.paths.contains(tempDir));
+        assertTrue(args.paths.contains(tempDir));
     }
 
     @Test
     @DisplayName("Tests when multiple Paths are passed in")
     void testMultiplePaths(@TempDir Path tempDir) throws IOException {
-        Assertions.assertTrue(Files.isDirectory(tempDir));
+        assertTrue(Files.isDirectory(tempDir));
         Path childTempDir = Files.createTempDirectory(tempDir, null);
-        Assertions.assertTrue(Files.isDirectory(childTempDir));
+        assertTrue(Files.isDirectory(childTempDir));
         Args args = new Args();
         JCommander.newBuilder().addObject(args).build().parse(tempDir.toString(), childTempDir.toString());
-        Assertions.assertTrue(args.paths.stream().allMatch(p -> p.equals(tempDir) || p.equals(childTempDir)));
+        assertTrue(args.paths.stream().allMatch(p -> p.equals(tempDir) || p.equals(childTempDir)));
     }
 
     @Test
@@ -48,7 +51,7 @@ class ArgsTest {
                 JCommander.newBuilder().addObject(args).build().parse("/tmp/___args_test___")
         );
 
-        Assertions.assertTrue(exception.getMessage().contains("No such file or paths"));
+        assertTrue(exception.getMessage().contains("No such file or paths"));
     }
 
 }
