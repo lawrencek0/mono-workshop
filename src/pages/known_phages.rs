555eb8f0aea86d5696b871b96fce600d322caef5
lawrencek0 <l.khadka@outlook.com>
2019-08-18 19:33:47 -0600

    expiremental recursive scraping function

--- src/pages/known_phages.rs
+++ src/pages/known_phages.rs
@@ -1,13 +1,12 @@
 use super::common;
 use fantoccini::{error, Client, Locator};
-use futures::future::Future;
+use futures::future::{Future, loop_fn, Loop};
 
 /// Opens the page containing all the phages for the given genus
 pub fn open_genus<'a>(
     client: Box<impl Future<Item = Client, Error = error::CmdError>>,
     genus: common::Genera,
-) -> Box<impl Future<Item = Client, Error = error::CmdError>> {
-    Box::new(
+) -> impl Future<Item = Client, Error = error::CmdError> {
         client
             .and_then(|c| c.wait_for_find(Locator::Css(r#"a[href="known_phage_visualization"]"#)))
             .and_then(|e| e.click())
@@ -27,6 +26,30 @@ pub fn open_genus<'a>(
             .and_then(|mut c| c.find(Locator::Css("#submit")))
             .and_then(|e| e.click())
             .and_then(|c| c.wait_for_find(Locator::Css(r#"select[name="cutTable_length"]"#)))
-            .and_then(|e| e.select_by_value("100")),
-    )
+            .and_then(|e| e.select_by_value("100"))
+}
+
+pub fn scrape_phage(client: std::sync::Arc<impl Future<Item = Client, Error = error::CmdError>>)-> Box<impl futures::Future<Item = Vec<fantoccini::Element>, Error = error::CmdError>> {
+    let phages = client.and_then(|mut c| {
+        c.find_all(Locator::Css(r#"tr[id^="phage"]"#))
+    });
+
+    Box::new(phages)
+}
+
+// maybe create a struct for holding phages?
+pub fn scrape_phages(client: std::sync::Arc<impl Future<Item = Client, Error = error::CmdError>>)-> impl Future<Item = Vec<String>, Error = error::CmdError> {
+    let scrapes = loop_fn(Vec::new(), |c| {
+        scrape_phage(client.clone()).and_then(|phages| {
+
+            if let e = client.and_then(|c| c.find(Locator::Css(r#"a#cutTable_next"#))) {
+                // click on the thing
+                Ok(Loop::Continue(client))
+            } else {
+                Ok(Loop::Break(client))
+            }
+            // map err element not found then call fn else break out of it?
+        })
+    });
+    scrapes
 }
