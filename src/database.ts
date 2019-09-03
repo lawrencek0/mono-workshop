c7a31227966196fa7a5da6d1e9f6ceaa9c61baed
lawrencek0 <l.khadka@outlook.com>
2019-09-02 22:22:32 -0500

    create database and table on startup

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,6 @@
-import mysql from "mysql2/promise";
-import { database } from "./util/secrets";
+import mysql from 'mysql2/promise';
+import { database } from './util/secrets';
+import logger from './util/logger';
 
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -8,5 +9,23 @@ const pool = mysql.createPool({
     password: database.MYSQL_PASSWORD,
     port: Number.parseInt(database.MYSQL_PORT, 10)
 });
+
+async function main() {
+    try {
+        await pool.query('CREATE DATABASE IF NOT EXISTS system');
+        await pool.query('USE system');
+        await pool.query(`CREATE TABLE IF NOT EXISTS users (
+                id int(10) unsigned NOT NULL AUTO_INCREMENT,
+                username varchar(255) NOT NULL,
+                password varchar(255) NOT NULL,
+                PRIMARY KEY (id),
+                UNIQUE KEY username (username)
+            )`);
+    } catch (e) {
+        logger.log('error', 'An error occured while setting up MySQL Connection ' + e);
+    }
+}
+
+main();
 
 export default pool;
