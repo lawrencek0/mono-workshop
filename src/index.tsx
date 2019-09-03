a9aa48a61ee3379dbb7115596eb036dbd4a84c77
lawrencek0 <l.khadka@outlook.com>
2019-09-03 11:57:33 -0500

    use typographyjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import Typography from 'typography';
+import noriegaTheme from 'typography-theme-noriega';
 import 'tachyons';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const typography = new Typography(noriegaTheme);
+typography.injectStyles();
 
 ReactDOM.render(
     <ThemeProvider theme={{ mode: 'light' }}>
---
+++ src/types/noriega-theme.d.ts
@@ -0,0 +1,6 @@
+declare module 'typography-theme-noriega' {
+    import { TypographyOptions } from 'typography';
+    const Theme: TypographyOptions;
+
+    export = Theme;
+}
