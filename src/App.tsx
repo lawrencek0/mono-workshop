18a3058d6e4a7c4ee4b5b3d4ea190e4d605d22ff
lawrencek0 <l.khadka@outlook.com>
2019-09-03 00:42:52 -0500

    create signup component for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './login/Login';
+import SignUp from './login/Signup';
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Login />
+            <SignUp />
         </div>
     );
 };
