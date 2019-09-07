b473955d319d78239c5c6d17565436d5367f9e69
lawrencek0 <l.khadka@outlook.com>
2019-09-07 18:32:07 -0500

    remove signup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Login from './login/Login';
-import SignUp from './login/Signup';
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Login />
-            <SignUp />
         </div>
     );
 };
