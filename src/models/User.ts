2178744933a71f358b84b1899ce5038bb2f88927
lawrencek0 <l.khadka@outlook.com>
2019-09-07 12:42:27 -0500

    create model for saving User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,38 @@
-class User {
-    public email: string;
-    public password: string;
+import db from '../database';
 
-    constructor(email: string, password: string) {
-        this.email = email;
-        this.password = password;
+interface IUser {
+    username: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    role: 'student' | 'faculty' | 'admin';
+    cwid: number;
+}
+
+class User {
+    public static getUser(id: number) {
+        return db.query('SELECT * FROM users WHERE `id` = ?', [id]);
+    }
+
+    public static getUserWithUsername(username: string) {
+        return db.query('SELECT * FROM users WHERE `username`= ?', [username]);
+    }
+
+    public static save(user: IUser) {
+        // @TODO: hash the password before saving!!
+        return db.query(
+            'INSERT INTO users (username, password, first_name, last_name, email, role, cwid) VALUES(?, ?, ?, ?, ?, ?, ?)',
+            [
+                user.username,
+                user.password,
+                user.first_name,
+                user.last_name,
+                user.email,
+                user.role,
+                user.cwid
+            ]
+        );
     }
 }
 
