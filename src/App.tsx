89867d4099556c47319b774ffccf7f1c9eab04d4
lawrencek0 <l.khadka@outlook.com>
2019-09-11 22:09:36 -0500

    style main body of the app with toggle for theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import Login from './login/Login';
 import SignUp from './login/Signup';
 
 const App: React.FC = () => {
-    const [theme] = useState('light');
+    const [theme, setTheme] = useState<theme.themes>('light');
+
+    const toggleTheme = (): void => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
 
     return (
+        // ThemeProvider provides the theme for all our components
         <ThemeProvider theme={{ mode: theme }}>
             <Main>
+                <button onClick={toggleTheme}>Toggle Theme</button>
                 <Login />
                 <SignUp />
             </Main>
@@ -17,6 +27,9 @@ const App: React.FC = () => {
     );
 };
 
+// Use styled-componentes to generate the `Main` Component The attrs is used to
+// use Tachyon's styling. By using colors from the theme, this component becomes
+// flexible enough to switch between dark and nigh mode.
 const Main = styled.main.attrs(() => ({
     className: 'ma2',
 }))`
