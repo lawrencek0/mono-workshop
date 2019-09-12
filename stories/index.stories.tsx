165b1986a2bc0aa2518fb132425021870013fad7
lawrencek0 <l.khadka@outlook.com>
2019-09-11 20:57:46 -0500

    add first story from Login component

---
+++ stories/index.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import Login from '../src/login/Login';
+
+storiesOf('Login', module).add('to Storybook', () => <Login />);
