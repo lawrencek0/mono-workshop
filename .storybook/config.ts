de5301fc12e651230cbed4f534efd367649cfea2
lawrencek0 <l.khadka@outlook.com>
2019-09-15 01:20:08 -0500

    add decorator for styled components

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,12 +1,12 @@
 import { configure, addDecorator } from '@storybook/react';
-import { withInfo } from "@storybook/addon-info";
+import { withInfo } from '@storybook/addon-info';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 
 function loadStories() {
-  addDecorator(withInfo);
-  req.keys().forEach(req);
+    addDecorator(withInfo);
+    req.keys().forEach(req);
 }
 
 configure(loadStories, module);
