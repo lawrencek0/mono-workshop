f70b9529e4800436b8b82fbd2156163f827409cb
sujeong0826 <sujeong0826@gmail.com>
2019-09-19 15:09:33 -0500

    created appointment and updated user table

--- src/database.ts
+++ src/database.ts
@@ -4,54 +4,65 @@ import { database } from './util/secrets';
 import logger from './util/logger';
 
 class Database {
-    private readonly pool: mysql.Pool;
-    private readonly queryPromise: mysql.QueryFunction;
+  private readonly pool: mysql.Pool;
+  private readonly queryPromise: mysql.QueryFunction;
 
-    constructor(config: mysql.PoolConfig) {
-        this.pool = mysql.createPool(config);
-        this.queryPromise = promisify(this.pool.query).bind(this.pool);
-    }
+  constructor(config: mysql.PoolConfig) {
+    this.pool = mysql.createPool(config);
+    this.queryPromise = promisify(this.pool.query).bind(this.pool);
+  }
 
-    async query(query: string, values?: any[]): Promise<mysql.Query> {
-        try {
-            const escapedQuery = mysql.format(query, values);
-            return this.queryPromise(escapedQuery, values);
-        } catch (e) {
-            logger.log('error', `Problem querying the database: ${e}`);
-        }
+  async query(query: string, values?: any[]): Promise<mysql.Query> {
+    try {
+      const escapedQuery = mysql.format(query, values);
+      return this.queryPromise(escapedQuery, values);
+    } catch (e) {
+      logger.log('error', `Problem querying the database: ${e}`);
     }
+  }
 
-    async beforeAll() {
-        try {
-            await this.query('CREATE DATABASE IF NOT EXISTS system');
-            await this.query('USE system');
-            await this.query(`CREATE TABLE IF NOT EXISTS users (
-                    id int(10) unsigned NOT NULL AUTO_INCREMENT,
-                    username varchar(255) NOT NULL UNIQUE,
-                    password varchar(255) NOT NULL,
-                    first_name varchar(50) NOT NULL,
-                    last_name varchar(50) NOT NULL,
-                    email varchar(255) NOT NULL UNIQUE,
-                    role ENUM('student', 'faculty', 'admin') NOT NULL,
-                    cwid int(8) NOT NULL UNIQUE,
-                    PRIMARY KEY (id)
-                )`);
-        } catch (e) {
-            logger.log(
-                'error',
-                'An error occured while setting up MySQL Connection ' + e
-            );
-        }
+  async beforeAll() {
+    try {
+      await this.query('CREATE DATABASE IF NOT EXISTS system');
+      await this.query('USE system');
+      await this.query(`CREATE TABLE IF NOT EXISTS User (
+                user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                email VARCHAR(100) NOT NULL,
+                username VARCHAR(50) NOT NULL,
+                role ENUM('student', 'faculty', 'admin') NOT NULL,
+                PRIMARY KEY (user_id),
+                UNIQUE (email),
+                UNIQUE (username)
+              );`);
+      await this.query(`CREATE TABLE IF NOT EXISTS Appointment (
+                appoint_id INT NOT NULL AUTO_INCREMENT,
+                student_id INT(8) UNSIGNED NULL,
+                name VARCHAR(50) NOT NULL,
+                appoint_date_start DATETIME NOT NULL,
+                faculty_id INT(8) UNSIGNED NOT NULL,
+                appoint_date_end DATETIME NOT NULL,
+                PRIMARY KEY (appoint_id),
+                FOREIGN KEY (student_id) REFERENCES User(user_id),
+                FOREIGN KEY (faculty_id) REFERENCES User(user_id),
+                UNIQUE (student_id, faculty_id)
+              );`);
+    } catch (e) {
+      logger.log(
+        'error',
+        'An error occured while setting up MySQL Connection ' + e
+      );
     }
+  }
 }
 
-
 const db = new Database({
-    connectionLimit: 10,
-    host: database.MYSQL_HOSTNAME,
-    user: database.MYSQL_USER,
-    password: database.MYSQL_PASSWORD,
-    port: Number.parseInt(database.MYSQL_PORT, 10)
+  connectionLimit: 10,
+  host: database.MYSQL_HOSTNAME,
+  user: database.MYSQL_USER,
+  password: database.MYSQL_PASSWORD,
+  port: Number.parseInt(database.MYSQL_PORT, 10)
 });
 db.beforeAll();
 
