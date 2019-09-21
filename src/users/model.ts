91f7319cf6ae0f4df8c318a4a2d97237d4b0db4a
lawrencek0 <l.khadka@outlook.com>
2019-09-21 15:55:23 -0500

    add plain object for User

---
+++ src/users/model.ts
@@ -0,0 +1,14 @@
+export type Role = 'student' | 'faculty' | 'admin';
+
+class User {
+    constructor(
+        public id: number,
+        public username: string,
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public role: Role
+    ) {}
+}
+
+export { User as UserModel };
