72b74b7be101aab3638b17cbbb842a37e2cc3f37
lawrencek0 <l.khadka@outlook.com>
2019-09-21 15:54:48 -0500

    crearte specific router for each feature

---
+++ src/auth/router.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import * as userController from './controller';
+
+const router = Router();
+
+router.post('/login', userController.validateLogin, userController.postLogin);
+router.post(
+    '/signup',
+    userController.validateSignup,
+    userController.postSignup
+);
+
+export { router as AuthRouter };
