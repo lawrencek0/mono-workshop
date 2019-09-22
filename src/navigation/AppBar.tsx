532ed7c086e275285e83d565b950f06a14c418b7
lawrencek0 <l.khadka@outlook.com>
2019-09-22 15:38:08 -0500

    create AppBar component for smaller devices

---
+++ src/navigation/AppBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { sidebarBackgroundColor } from './theme';
+import { BaseVariant, primaryTextColor } from 'theme';
+import { MdMenu } from 'react-icons/md';
+import { Link } from '@reach/router';
+
+const AppBar: React.FC<{}> = () => {
+    return (
+        <Header variant="primary">
+            <h1 className="f4">Team Yellow</h1>
+            <Menu />
+        </Header>
+    );
+};
+
+const Menu: React.FC<{}> = () => {
+    return (
+        <StyledLink to="/menu">
+            <StyledIcon />
+        </StyledLink>
+    );
+};
+
+const StyledLink = styled(Link).attrs(() => ({
+    className: 'ml-auto',
+}))`
+    color: ${primaryTextColor};
+`;
+
+const StyledIcon = styled(MdMenu).attrs(() => ({
+    className: 'f2',
+}))``;
+
+const Header = styled.header.attrs(() => ({
+    className: 'flex items-center ph2 f4',
+}))<{ variant: Extract<BaseVariant, 'primary'> }>`
+    background-color: ${sidebarBackgroundColor};
+    color: ${primaryTextColor};
+`;
+
+export { AppBar as default };
