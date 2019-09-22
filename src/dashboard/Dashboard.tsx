96a8c1155984496f833cacba73867b211b4e46f2
lawrencek0 <l.khadka@outlook.com>
2019-09-22 12:04:54 -0500

    use the Content component instead

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -3,12 +3,13 @@ import styled from 'styled-components/macro';
 import { RouteComponentProps } from '@reach/router';
 import * as theme from 'theme';
 import { useAuthDispatch, logout } from 'auth/hooks';
+import { Content } from 'navigation/Content';
 
 const Dashboard: React.FC<RouteComponentProps> = () => {
     const dispatch = useAuthDispatch();
 
     return (
-        <Main>
+        <Content>
             <button
                 onClick={() => {
                     logout(dispatch);
@@ -16,18 +17,8 @@ const Dashboard: React.FC<RouteComponentProps> = () => {
             >
                 logout
             </button>
-        </Main>
+        </Content>
     );
 };
-
-// Use styled-componentes to generate the `Main` Component The attrs is used to
-// use Tachyon's styling. By using colors from the theme, this component becomes
-// flexible enough to switch between dark and nigh mode.
-const Main = styled.main.attrs(() => ({
-    className: 'ma2',
-}))<RouteComponentProps>`
-    background-color: ${theme.backgroundColor};
-    color: ${theme.primaryTextColor};
-`;
 
 export { Dashboard };
