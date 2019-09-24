3909e9b8355cfc8f8efb496861556d19e3af32fb
sujeong0826 <sujeong0826@gmail.com>
2019-09-24 15:17:37 -0500

    Updating router, only validated user will access

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { AuthRouter } from './auth/router';
 import { AppointmentRouter } from './appointments/router';
+import { validate } from './auth/controller';
 
 // Route handlers
 
 const router = Router();
 
 router.use('/auth', AuthRouter);
-router.use('/appointments', AppointmentRouter);
+router.use('/appointments', validate, AppointmentRouter);
 export default router;
