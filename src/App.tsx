b97a9daa5afda051b5db87db947d5bb8e65a73bd
lawrencek0 <l.khadka@outlook.com>
2019-09-25 22:30:31 -0500

    add events page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import Login from 'login/Login';
 import { Router, RouteComponentProps, Link } from '@reach/router';
 import { Dashboard } from 'dashboard/Dashboard';
 import { RouteGuard } from 'routing/PrivateRoute';
 import { Logout } from 'login/Logout';
 
+const EventPage = lazy(() => import('event/Event'));
+
 const App: React.FC = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Router>
                 <RouteGuard as={Dashboard} action="dashboard:visit" path="/" />
+                <RouteGuard as={EventPage} action="events:visit" path="events/*" />
                 <RouteGuard as={Login} action="login" path="/login" />
                 <Logout path="/logout" />
                 <NotFoundPage path="*" />
