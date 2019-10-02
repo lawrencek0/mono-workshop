1d81e91406325ced5103fb2c22d6c486ac2d0026
lawrencek0 <l.khadka@outlook.com>
2019-10-01 20:27:30 -0500

    update Events page with Dropdown

--- src/event/Event.tsx
+++ src/event/Event.tsx
@@ -1,16 +1,71 @@
-import React, { lazy } from 'react';
+import React, { lazy, useRef, useState, useEffect } from 'react';
 import { RouteComponentProps, Link, Router } from '@reach/router';
 import { Main } from 'navigation/Main';
 import { RouteGuard } from 'routing/PrivateRoute';
 
-const EventForm = lazy(() => import('./EventForm'));
+const EventForm = lazy(() => import('./AppointmentForm'));
+
+const Dropdown: React.FC<{ className?: string }> = ({ className }) => {
+    const node = useRef<HTMLDivElement>(null);
+    const [open, setOpen] = useState(false);
+
+    const handleClickOutside = (e: MouseEvent): void => {
+        if (node.current && node.current.contains(e.target as HTMLElement)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [open]);
+
+    return (
+        <div className={className} role="listbox" ref={node} aria-expanded={open} aria-label="Create New Event">
+            <button onClick={_e => setOpen(!open)}>+ New</button>
+            {open && (
+                <ul>
+                    <li role="option" aria-selected={false}>
+                        <Link to="add/appointment/1">Appointment</Link>
+                    </li>
+                    <li role="option" aria-selected={false}>
+                        <Link to="add/event">Events</Link>
+                    </li>
+                    <li role="option" aria-selected={false}>
+                        <Link to="add/reminder">Reminder</Link>
+                    </li>
+                </ul>
+            )}
+        </div>
+    );
+};
+
+const Page: React.FC<RouteComponentProps> = () => {
+    return (
+        <div className="flex">
+            <div className="f1">Events</div>
+            <div className="ml-auto">
+                <Dropdown />
+            </div>
+        </div>
+    );
+};
 
 const Event: React.FC<RouteComponentProps> = () => {
     return (
         <Main>
-            <Link to="add/1">Add</Link>
             <Router>
-                <RouteGuard as={EventForm} action="events:add" path="add/:step"></RouteGuard>
+                <RouteGuard as={Page} action="events:visit" path="/"></RouteGuard>
+                <RouteGuard as={EventForm} action="events:add" path="add/appointment/:step"></RouteGuard>
             </Router>
         </Main>
     );
