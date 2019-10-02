48081ff330d48da3c1cfa45e50f5ae7dd7204179
Sujeong Jang <sujeong0826@gmail.com>
2019-10-01 23:30:19 -0500

    Fixing "ConnectionNotFoundError" - Having gitConnection  assigned to two varaibles causes the program to crash :(

--- src/appointment/service.ts
+++ src/appointment/service.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
 import { Detail } from './detail/models';
-import logger from '../util/logger';
 import { getConnection } from 'typeorm';
 import { Slot } from './slot/models';
 
-const detailRepo = getConnection().getRepository(Detail);
-const slotRepo = getConnection().getRepository(Slot);
-
 export const create = async (req: Request, res: Response) => {
-    const slots = slotRepo.create(req.body.dates);
-    const detail = detailRepo.create({ title: req.body.title, description: req.body.description, slots });
+    const slots = getConnection()
+        .getRepository(Slot)
+        .create(req.body.dates);
+    const detail = getConnection()
+        .getRepository(Detail)
+        .create({ title: req.body.title, description: req.body.description, slots });
 
-    res.send({ slots, detail });
+    res.send({ detail });
 };
