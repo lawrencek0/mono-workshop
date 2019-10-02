b5f25952aef20f7d5f575674fafc5492c0a88619
alexLau <alexlau2014@gmail.com>
2019-10-02 15:45:45 -0500

    created a service for Users

---
+++ src/user/service.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { User } from './model';
+import { getConnection } from 'typeorm';
+
+export const listAllUsers = async (req: Request, res: Response) => {
+    const user = await getConnection()
+        .getRepository(User)
+        .query('SELECT * FROM user');
+
+    res.send({ user });
+};
