6649005b2faf052031492d551a3c8327442545c5
lawrencek0 <l.khadka@outlook.com>
2019-10-03 10:39:11 -0500

    save faculty id while saving appointment_details

--- src/appointment/service.ts
+++ src/appointment/service.ts
@@ -6,6 +6,8 @@ import { User } from '../user/model';
 import hashids from '../util/hasher';
 
 export const create = async (req: Request, res: Response) => {
+    const maskedId = res.locals.user['custom:user_id'];
+    const id = (hashids.decode(maskedId)[0] as unknown) as number;
     const slots = await getConnection()
         .getRepository(Slot)
         .save(req.body.dates);
@@ -18,9 +20,12 @@ export const create = async (req: Request, res: Response) => {
             .getRepository(User)
             .save(user);
     });
+    const faculty = await getConnection()
+        .getRepository(User)
+        .findOne(id);
     const detail = await getConnection()
         .getRepository(Detail)
-        .save({ title: req.body.title, description: req.body.description, slots });
+        .save({ title: req.body.title, description: req.body.description, slots, user: faculty });
 
     res.send({ detail });
 };
