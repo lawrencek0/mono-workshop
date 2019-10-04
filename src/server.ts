af50d37d6830d88e4eba29dfbcf3f373573c5c41
alexLau <alexlau2014@gmail.com>
2019-10-04 16:48:44 -0500

    Changed our DB to keep the data on start up

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,20 @@
 import errorHandler from 'errorhandler';
 
 import app from './app';
-import { createConnection } from 'typeorm';
-import { database } from './util/secrets';
-import logger from './util/logger';
-import { User } from './user/model';
-import { Slot } from './appointment/slot/models';
-import { Detail } from './appointment/detail/models';
+import databaseConn from './loader/database';
 
-createConnection({
-    type: 'mysql',
-    host: database.MYSQL_HOSTNAME,
-    port: parseInt(database.MYSQL_PORT),
-    username: database.MYSQL_USER,
-    password: database.MYSQL_PASSWORD,
-    database: database.DATABASE,
-    entities: [User, Slot, Detail],
-    synchronize: true,
-    logging: true,
-})
-    .then(async () => {
-        /**
-         * Error Handler. Provides full stack - remove for production
-         */
-        app.use(errorHandler());
-        /**
-         * Start Express server.
-         */
-        app.listen(app.get('port'), () => {
-            console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
-            console.log('  Press CTRL-C to stop\n');
-        });
-    })
-    .catch(error => logger.error(error));
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
+/**
+ * Start Express server.
+ */
+databaseConn();
+
+app.listen(app.get('port'), () => {
+    console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
+});
 
 export default app;
