75392532a00d265f87e409287cb0375f2b3ce584
lawrencek0 <l.khadka@outlook.com>
2019-10-05 21:38:48 -0500

    remove remaining tachyon classes

--- src/navigation/Main.tsx
+++ src/navigation/Main.tsx
@@ -3,7 +3,7 @@ import { RouteComponentProps } from '@reach/router';
 import tw from 'tailwind.macro';
 import { backgroundColor, primaryTextColor, media } from 'theme';
 import styled from 'styled-components/macro';
-import { primaryRoutes, secondayRoutes } from 'routing/routes';
+import { primaryRoutes } from 'routing/routes';
 import { Sidebar } from './Sidebar';
 import { AppBar } from './AppBar';
 
@@ -12,29 +12,21 @@ export const Main: React.FC<{ children?: ReactNode } & RouteComponentProps> = ({
         <Wrapper>
             <Sidebar routes={primaryRoutes} />
             <div>
-                <StyledAppBar />
+                <AppBar />
                 <Content>{children}</Content>
             </div>
         </Wrapper>
     );
 };
 
-// Use styled-componentes to generate the `Main` Component The attrs is used to
-// use Tachyon's styling. By using colors from the theme, this component becomes
-// flexible enough to switch between dark and nigh mode.
-const Content = styled.main.attrs(() => ({
-    className: 'mh2 mh0-l mt2',
-}))`
-    flex-grow: 1;
+const Content = styled.main`
+    ${tw`mx-2 mt-2`}
     background-color: ${backgroundColor};
     color: ${primaryTextColor};
 `;
 
-const StyledAppBar = styled(AppBar)``;
-
-const Wrapper = styled.div.attrs(() => ({
-    className: 'flex flex-column-reverse flex-row-l',
-}))`
+const Wrapper = styled.div`
+    ${tw`flex flex-col-reverse md:flex-row`}
     @supports (display: grid) {
         ${media.desktop} {
             display: grid;
