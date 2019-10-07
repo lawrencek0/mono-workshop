e5b9ac445ff631d5e83b2fae98167fc4b770a40c
lawrencek0 <l.khadka@outlook.com>
2019-10-06 20:27:34 -0500

    create sidebar for holding event cards

---
+++ src/event/dashboard/Sidebar.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import tw from 'tailwind.macro';
+import styled from 'styled-components/macro';
+import { Menu, MenuLink, MenuButton } from '@reach/menu-button';
+import { StyledMenuList } from 'navigation/Navbar';
+import { Link } from '@reach/router';
+import { FaCaretDown } from 'react-icons/fa';
+
+type Props = {
+    className?: string;
+};
+
+const NewEvent: React.FC<{}> = () => {
+    return (
+        <Menu>
+            <StyledMenuBtn>
+                Create New
+                <FaCaretDown />
+            </StyledMenuBtn>
+            <StyledMenuList>
+                <MenuLink as={(Link as unknown) as string} to="appointment/new/1">
+                    Appointment
+                </MenuLink>
+            </StyledMenuList>
+        </Menu>
+    );
+};
+
+const Sidebar: React.FC<Props> = ({ className }) => {
+    return (
+        <Wrapper className={className}>
+            <Title>Events</Title>
+            <NewEvent />
+        </Wrapper>
+    );
+};
+
+const StyledMenuBtn = styled(MenuButton)`
+    ${tw`flex items-center font-light mb-4`}
+`;
+
+const Title = styled.h1`
+    ${tw`font-bold text-3xl mb-2`}
+`;
+
+const SubTitle = styled.h2`
+    ${tw`text-lg text-gray-700 mb-2`}
+`;
+
+const Wrapper = styled.div`
+    ${tw`flex flex-col px-8`}
+`;
+
+export default Sidebar;
