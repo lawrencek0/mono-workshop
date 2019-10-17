b8f421a67b5a0edaa7493823672b0946cd385dc5
lawrencek0 <l.khadka@outlook.com>
2019-10-16 22:29:09 -0500

    create html table with slots using moment

--- src/controllers/email.ts
+++ src/controllers/email.ts
@@ -1,7 +1,10 @@
 import AWS from 'aws-sdk';
 import nodemailer from 'nodemailer';
+import moment from 'moment';
+import 'moment-timezone';
 import { Request, Response } from 'express';
 import { email } from '../util/secrets';
+import { Detail } from '../entities/Detail';
 
 AWS.config.update({
     accessKeyId: email.AWS_ACCESS_KEY_ID,
@@ -13,24 +16,67 @@ const transporter = nodemailer.createTransport({
     SES: new AWS.SES({ apiVersion: '2010-12-01' }),
 });
 
+type table = 'appointment' | 'event';
+
+const generateHtmlMsg = (type: table, events: Detail): string => {
+    let output = `<h1>${events.title}</h1>`;
+    output += `<div>${events.description}</div>`;
+    output += '<table>';
+    if (type === 'appointment') {
+        output += `<caption>Time Slots for ${events.title}</caption>`;
+        output += `<thead><tr>
+                        <th>Day</th>
+                        <th>Start</th>
+                        <th>End</th>
+                    </tr></thead>`;
+        output += '<tbody>';
+        events.slots.forEach(({ start, end }) => {
+            output += `
+            <tr>
+                <td>${moment(start)
+                    .tz('America/Chicago')
+                    .format('MMM Do')}
+                </td>
+                <td>${moment(start)
+                    .tz('America/Chicago')
+                    .format('hh:mm')}
+                </td>
+                <td>${moment(end)
+                    .tz('America/Chicago')
+                    .format('hh:mm')}
+                </td>
+            </tr>`;
+        });
+        output += '</tbody>';
+    }
+
+    output += '</table>';
+
+    return output;
+};
+
 // @TODO need to allow for 2+ attachment to be sent on a single email
 export const sender = async (req: Request, res: Response) => {
     try {
-        transporter.sendMail({
+        const type: table = req.body.type;
+        const events: Detail = req.body.events;
+        const html = generateHtmlMsg(type, events);
+        const result = await transporter.sendMail({
             from: email.SOURCE_EMAIL,
-            to: req.body.receiver,
-            subject: req.body.subj,
-            text: req.body.text,
-            alternatives: [
-                {
-                    filename: req.body.filename,
-                    contentType: req.body.contentType,
-                    content: req.body.content,
-                },
-            ],
+            to: req.body.to,
+            subject: req.body.subject,
+            text: req.body.body,
+            html,
+            // alternatives: [
+            //     {
+            //         filename: req.body.filename,
+            //         contentType: req.body.contentType,
+            //         content: req.body.content,
+            //     },
+            // ],
         });
-        res.send('Email successfully sent into the void');
+        res.send({ msg: 'Email successfully sent into the void', result });
     } catch (err) {
-        res.send('Mistakes were made ' + err);
+        res.status(403).send({ msg: `Mistakes were made ${err}` });
     }
 };
