baa37c3f298b11d313e5f5b4ca8f0076feef189c
lawrencek0 <l.khadka@outlook.com>
2019-10-24 23:32:25 -0500

    hook up validation with proper styling

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -4,7 +4,8 @@ import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
 import { login, useAuthDispatch } from 'auth/hooks';
 import { localStorageKey } from 'utils/storage';
-import { useFormik, Formik, Form, Field } from 'formik';
+import { useFormik, FieldMetaProps } from 'formik';
+import { MdInfo } from 'react-icons/md';
 import {
     FormWrapper,
     FormTitle,
@@ -13,30 +14,23 @@ import {
     StyledLabel,
     StyledInput,
     InputWrapper,
+    Variant,
+    InputErrorMsg,
 } from 'shared/inputs';
-import { UserPayload } from './types';
 
-const validate = (values: any): any => {
-    const errors: any = {};
-
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Za-z0-9]@(warhawks.ulm.edu|ulm.edu)/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
-
-    // other code goes here
-
-    return errors;
-};
 const Login: React.FC<RouteComponentProps & { to?: string; replace?: boolean }> = ({ to = '/', replace = false }) => {
+    const dispatch = useAuthDispatch();
+    const [rememberUser, setRememberUser] = useState(
+        localStorage.getItem(localStorageKey('rememberMe')) === 'true' || false,
+    );
+
     // @TODO: handle login failure
-    const { getFieldProps, submitForm, errors, touched } = useFormik({
+    const { getFieldProps, setSubmitting, handleSubmit } = useFormik({
         initialValues: {
-            email: '',
+            email: localStorage.getItem(localStorageKey('email')) || '',
             password: '',
         },
-        validate: (values: any): any => {
+        validate: values => {
             const errors = {
                 email: '',
                 password: '',
@@ -44,84 +38,77 @@ const Login: React.FC<RouteComponentProps & { to?: string; replace?: boolean }> 
 
             if (!values.email) {
                 errors.email = 'Required';
-            } else if (!/^[A-Z0-9._%+-]+@[warhawks.ulm.edu]||[ulm.edu]/i.test(values.email)) {
+            } else if (!/^[A-Z0-9]+@(warhawks.ulm|ulm).edu$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
+            }
+
+            if (!values.password) {
+                errors.password = 'Required';
             }
 
             return errors;
         },
-        onSubmit: (values: any) => {
-            console.log(values);
+        onSubmit: async values => {
+            try {
+                await login(dispatch, values);
+                if (rememberUser) {
+                    localStorage.setItem(localStorageKey('email'), values.email);
+                    localStorage.setItem(localStorageKey('rememberMe'), 'true');
+                } else {
+                    localStorage.removeItem(localStorageKey('email'));
+                    localStorage.removeItem(localStorageKey('rememberMe'));
+                }
+                if (!to.includes('login')) {
+                    navigate(to, { replace });
+                }
+            } catch (e) {
+                setSubmitting(false);
+            }
         },
     });
-    const [email, metadataEmail] = getFieldProps({ name: 'email', type: 'text' });
-    const [password, metadataPassword] = getFieldProps({ name: 'password', type: 'password' });
-    const [inputs, setInputs] = useState({
-        email: localStorage.getItem(localStorageKey('email')) || '',
-        password: '',
-    });
-    const [rememberUser, setRememberUser] = useState(
-        localStorage.getItem(localStorageKey('rememberMe')) === 'true' || false,
-    );
 
-    const dispatch = useAuthDispatch();
-
-    const HandleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<UserPayload | void> => {
-        if (e) {
-            e.preventDefault();
-        }
-
-        if (inputs.email === '' || inputs.password === '') return;
-
-        // @TODO need better error handling
-        try {
-            await login(dispatch, inputs);
-        } catch (e) {
-            throw new Error(`There was a error logging in: ${e}`);
-        }
-        if (rememberUser) {
-            localStorage.setItem(localStorageKey('email'), inputs.email);
-            localStorage.setItem(localStorageKey('rememberMe'), 'true');
-        } else {
-            localStorage.removeItem(localStorageKey('email'));
-            localStorage.removeItem(localStorageKey('rememberMe'));
-        }
-        if (!to.includes('login')) {
-            navigate(to, { replace });
-        }
-    };
-
-    const handleInputChange = ({ currentTarget }: { currentTarget: HTMLInputElement }): void => {
-        setInputs(inputs => ({ ...inputs, [currentTarget.name]: currentTarget.value }));
-    };
+    const [email, metadataEmail] = getFieldProps({ name: 'email' });
+    const [password, metadataPassword] = getFieldProps({ name: 'password' });
 
     const handleCheckboxChange = (_: React.ChangeEvent<HTMLInputElement>): void => {
         setRememberUser(!rememberUser);
     };
 
+    const getVariant = (metadata: FieldMetaProps<unknown>): Variant => {
+        return metadata.touched && metadata.error ? 'danger' : 'default';
+    };
+
+    const isInvalid = (metaProps: FieldMetaProps<unknown>[]): boolean => {
+        return metaProps.some(meta => !meta.value || meta.error);
+    };
+
     return (
         <Wrapper>
-            <form onSubmit={HandleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <FormTitle>Login</FormTitle>
                 <InputWrapper>
-                    <StyledLabel htmlFor="email">Email</StyledLabel>
-                    <StyledInput
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={inputs.email}
-                        onChange={handleInputChange}
-                    />
+                    <StyledLabel htmlFor="email" variant={getVariant(metadataEmail)}>
+                        Email
+                    </StyledLabel>
+                    <StyledInput type="email" id="email" {...email} variant={getVariant(metadataEmail)} />
+                    {metadataEmail.touched && metadataEmail.error && (
+                        <InputErrorMsg>
+                            <StyledInfo />
+                            {metadataEmail.error}
+                        </InputErrorMsg>
+                    )}
                 </InputWrapper>
                 <InputWrapper>
-                    <StyledLabel htmlFor="password">Password</StyledLabel>
-                    <StyledInput
-                        type="password"
-                        name="password"
-                        id="password"
-                        value={inputs.password}
-                        onChange={handleInputChange}
-                    />
+                    <StyledLabel htmlFor="password" variant={getVariant(metadataPassword)}>
+                        Password
+                    </StyledLabel>
+                    <StyledInput type="password" id="password" {...password} variant={getVariant(metadataPassword)} />
+                    {metadataPassword.touched && metadataPassword.error && (
+                        <InputErrorMsg>
+                            <StyledInfo />
+                            {metadataPassword.error}
+                        </InputErrorMsg>
+                    )}
                 </InputWrapper>
                 <InputWrapper>
                     <input
@@ -134,12 +121,21 @@ const Login: React.FC<RouteComponentProps & { to?: string; replace?: boolean }> 
                     <StyledCheckboxLabel htmlFor="rememberMe">Remember Me</StyledCheckboxLabel>
                 </InputWrapper>
                 <InputWrapper>
-                    <StyledSubmitBtn type="submit" value="Login" />
+                    <StyledSubmitBtn
+                        type="submit"
+                        value="Login"
+                        disabled={isInvalid([metadataEmail, metadataPassword])}
+                        variant={isInvalid([metadataEmail, metadataPassword]) ? 'disabled' : 'default'}
+                    />
                 </InputWrapper>
             </form>
         </Wrapper>
     );
 };
+
+const StyledInfo = styled(MdInfo)`
+    ${tw`inline mr-1`}
+`;
 
 const Wrapper = styled(FormWrapper)`
     ${tw`lg:w-1/4`}
--- src/shared/inputs.tsx
+++ src/shared/inputs.tsx
@@ -1,7 +1,11 @@
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
+import theme from 'styled-theming';
 import tw from 'tailwind.macro';
 import { Link } from '@reach/router';
 import { Wrapper, Title } from './cards/styles';
+import { primaryBtnStyles, sharedBtnStyles, flatBtnStyles } from './buttons';
+
+export type Variant = 'default' | 'danger' | 'disabled';
 
 const FormWrapper = styled(Wrapper)`
     ${tw`w-full lg:w-2/3 m-auto my-4 text-xl`}
@@ -15,17 +19,53 @@ const InputWrapper = styled.div`
     ${tw`mb-4`}
 `;
 
-const StyledLabel = styled.label`
+const sharedLabelStyles = css`
     ${tw`text-gray-700 font-medium text-lg mb-2`}
+`;
+
+const labelStyles = theme.variants('mode', 'variant', {
+    default: {
+        light: css`
+            ${sharedLabelStyles}
+        `,
+    },
+    danger: {
+        light: css`
+            ${sharedLabelStyles}
+            ${tw`text-red-500`}
+        `,
+    },
+});
+
+const StyledLabel = styled.label<{ variant?: Variant }>`
+    ${labelStyles}
 `;
 
 const StyledCheckboxLabel = styled(StyledLabel)`
     ${tw`ml-2`}
 `;
 
-const StyledInput = styled.input`
+const sharedInputStyles = css`
     ${tw`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
         focus:outline-none focus:shadow-outline`}
+`;
+
+const inputStyles = theme.variants('mode', 'variant', {
+    default: {
+        light: css`
+            ${sharedInputStyles}
+        `,
+    },
+    danger: {
+        light: css`
+            ${sharedInputStyles}
+            ${tw`border-2 border-red-500 focus:border-red-700`}
+        `,
+    },
+});
+
+const StyledInput = styled.input<{ variant?: Variant }>`
+    ${inputStyles}
 `;
 
 const ButtonWrapper = styled(InputWrapper)`
@@ -37,10 +77,40 @@ const StyledLink = styled(Link)`
         hover:bg-primary-400 focus:outline-none focus:shadow-outline`}
 `;
 
-const StyledSubmitBtn = styled.input`
-    ${tw`bg-primary-400 hover:bg-primary-500 w-full font-bold py-2 px-4 rounded uppercase
-        focus:outline-none focus:shadow-outline cursor-pointer`}
+const submitBtnStyles = theme.variants('mode', 'variant', {
+    default: {
+        light: css`
+            ${primaryBtnStyles}
+            ${tw`font-bold cursor-pointer`}
+        `,
+    },
+    disabled: {
+        light: css`
+            ${flatBtnStyles};
+        `,
+    },
+});
+
+const StyledSubmitBtn = styled.input<{ variant?: Variant }>`
+    ${submitBtnStyles};
+    ${tw`w-full uppercase`}
 `;
+
+const InputErrorMsg = styled.p`
+    ${tw`text-red-500 pt-1 text-xs`}
+`;
+
+StyledLabel.defaultProps = {
+    variant: 'default',
+};
+
+StyledInput.defaultProps = {
+    variant: 'default',
+};
+
+StyledSubmitBtn.defaultProps = {
+    variant: 'default',
+};
 
 export {
     FormWrapper,
@@ -52,4 +122,5 @@ export {
     ButtonWrapper,
     StyledLink,
     StyledSubmitBtn,
+    InputErrorMsg,
 };
