b61ce193090a56e460bd151a86e8877356809f2c
sujeong0826 <sujeong0826@gmail.com>
2019-10-30 18:45:09 -0500

    Update events repository

---
+++ src/api/events/repository.ts
@@ -0,0 +1,23 @@
+import { Service } from 'typedi';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { Event } from './entity/Event';
+//import { User } from '../users/entity/User';
+
+@Service()
+export class SlotRepository {
+    @InjectRepository(Event)
+    private repository: Repository<Event>;
+
+    findAllStu(userId: number) {
+        return this.repository.find({ where: { student: userId }, relations: ['event'] });
+    }
+
+    findById(id: number) {
+        return this.repository.findOne({ where: { id: id } });
+    }
+
+    saveEvent(event: Event) {
+        return this.repository.save(event);
+    }
+}
