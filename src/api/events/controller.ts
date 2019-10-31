c61e78f4ca49fa1ed54e47a2bff8764fc0ab6328
sujeong0826 <sujeong0826@gmail.com>
2019-10-30 21:00:32 -0500

    Create event

--- src/api/events/controller.ts
+++ src/api/events/controller.ts
@@ -1,63 +1,42 @@
-import {
-    JsonController,
-    Post,
-    BodyParam,
-    Body,
-    HttpError,
-    CurrentUser,
-    Get,
-    Patch,
-    Param,
-} from 'routing-controllers';
-import { Inject } from 'typedi';
+import { JsonController, Post, Body, HttpError, CurrentUser } from 'routing-controllers';
 import { User } from '../users/entity/User';
-import { EventRepository } from './repository';
-import { Detail } from '../appointments/entity/Detail';
-import { Slot } from '../appointments/entity/Slot';
 import { EventColor } from '../events/entity/Color';
 import { Event } from './entity/Event';
+import { Repository, getRepository } from 'typeorm';
 
-@JsonController('/event')
+@JsonController('/events')
 export class EventController {
-    @Inject() private eventRepository: EventRepository;
+    private eventRepository: Repository<Event> = getRepository(Event);
+    private eventColorRepository: Repository<EventColor> = getRepository(EventColor);
+    private userRepository: Repository<User> = getRepository(User);
 
     @Post('/')
-    async create(
-        @CurrentUser({ required: true }) user: User, 
-        @Body() event: Event,
-        ) {
+    async create(@CurrentUser({ required: true }) creator: User, @Body() body: any) {
         try {
-            event.owner = user;
+            const users: User[] = await this.userRepository.findByIds(body.users.map(({ id }: { id: number }) => id));
+            const event = await this.eventRepository.save({
+                title: body.title,
+                start: body.start,
+                end: body.end,
+                description: body.description,
+                location: body.location,
+                owner: creator,
+                users,
+            });
+
+            const events = await Promise.all(
+                users.map(user => {
+                    const evnt = new EventColor();
+                    evnt.color = body.color;
+                    evnt.user = user;
+                    evnt.event = event;
+                    return evnt;
+                }),
+            );
+            await this.eventColorRepository.insert(events);
+            return event;
         } catch (e) {
             throw new HttpError(e);
         }
-    // }
+    }
 }
-
-// export const create = async (req: Request, res: Response) => {
-//     const maskedId = res.locals.user['custom:user_id'];
-//     const id = (hashids.decode(maskedId)[0] as unknown) as number;
-//     const creator = await getRepository(User).findOne(id);
-//     const users = await getRepository(User).findByIds(req.body.users.map(({ id }: { id: number }) => id));
-//     const event = await getRepository(Event).save({
-//         title: req.body.title,
-//         start: req.body.start,
-//         end: req.body.end,
-//         description: req.body.description,
-//         location: req.body.location,
-//         owner: creator,
-//         users,
-//     });
-
-//     const events = await Promise.all(
-//         users.map(user => {
-//             const evnt = new EventColor();
-//             evnt.color = req.body.color;
-//             evnt.user = user;
-//             evnt.event = event;
-//             return evnt;
-//         }),
-//     );
-//     await getRepository(EventColor).insert(events);
-//     res.send({ event });
-// };
