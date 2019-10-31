877fab42ed0b19460d93f5412eb16cc3c99fd8fc
sujeong0826 <sujeong0826@gmail.com>
2019-10-31 15:55:57 -0500

    Created EventColorRepository

--- src/api/events/repository.ts
+++ src/api/events/repository.ts
@@ -2,6 +2,7 @@ import { Service } from 'typedi';
 import { Repository } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { Event } from './entity/Event';
+import { EventColor } from './entity/Color';
 //import { User } from '../users/entity/User';
 
 @Service()
@@ -14,10 +15,27 @@ export class EventRepository {
     }
 
     findById(id: number) {
-        return this.repository.findOne({ where: { id: id } });
+        return this.repository.findOne({ where: { id }, relations: ['owner'] });
     }
-
     saveEvent(event: Event) {
         return this.repository.save(event);
+    }
+    deleteEvent(eventId: number) {
+        return this.repository.delete(eventId);
+    }
+}
+
+@Service()
+export class EventColorRepository {
+    @InjectRepository(EventColor)
+    private repository: Repository<EventColor>;
+    saveColors(color: EventColor[]) {
+        return this.repository.save(color);
+    }
+    deleteByEvent(event: Event) {
+        return this.repository.delete(event);
+    }
+    findById(id: number) {
+        return this.repository.findOne({ where: { id }, relations: ['event'] });
     }
 }
