a91e78b78543332155b9f07a04ac2ab8613ccb97
lawrencek0 <l.khadka@outlook.com>
2019-11-17 16:32:14 -0600

    open modal based on which day was clicked

--- src/calendar/dashboard/Dashboard.tsx
+++ src/calendar/dashboard/Dashboard.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'tailwind.macro';
 import styled from 'styled-components/macro';
 import Calendar from 'calendar/dashboard/Calendar';
+import { DateClickApi } from '@fullcalendar/core/Calendar';
+import { Modal } from './Modal';
 
 const Dashboard: React.FC<{}> = () => {
+    const [modalInfo, setModalInfo] = useState<{ left?: number; top?: number }>({});
+    const handleDateClick = ({ dayEl }: DateClickApi): void => {
+        const { clientHeight, clientWidth } = document.body;
+        const { left, right, top, bottom, width, height } = dayEl.getBoundingClientRect();
+        const x = left > clientWidth - right ? left - width : right;
+        const y = top > clientHeight - bottom ? top - height : bottom - height;
+
+        setModalInfo({ left: x, top: y });
+    };
     return (
         <Wrapper>
-            <StyledCalendar />
+            <Modal position={modalInfo} />
+            <StyledCalendar dateClick={handleDateClick} />
         </Wrapper>
     );
 };
