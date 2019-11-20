ad8bc11c1e644497855ee5745d45ca3d985f44fa
lawrencek0 <l.khadka@outlook.com>
2019-11-20 17:24:25 -0600

    dynamically position the modal based on click using refs

--- src/calendar/dashboard/Dashboard.tsx
+++ src/calendar/dashboard/Dashboard.tsx
@@ -1,24 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import tw from 'tailwind.macro';
 import styled from 'styled-components/macro';
 import Calendar from 'calendar/dashboard/Calendar';
-import { DateClickApi } from '@fullcalendar/core/Calendar';
 import { Modal } from './Modal';
+import FullCalendar from '@fullcalendar/react';
 
 const Dashboard: React.FC<{}> = () => {
-    const [modalInfo, setModalInfo] = useState<{ left?: number; top?: number }>({});
-    const handleDateClick = ({ dayEl }: DateClickApi): void => {
-        const { clientHeight, clientWidth } = document.body;
-        const { left, right, top, bottom, width, height } = dayEl.getBoundingClientRect();
-        const x = left > clientWidth - right ? left - width : right;
-        const y = top > clientHeight - bottom ? top - height : bottom - height;
+    const [modalInfo, setModalInfo] = useState<{ top?: number; left?: number }>({});
+    const calendarRef = useRef<FullCalendar>(null);
+    const modalRef = useRef<HTMLElement>(null);
 
-        setModalInfo({ left: x, top: y });
+    const calculateModalPos = (calendar: HTMLElement, cell: HTMLElement, modal: HTMLElement): typeof modalInfo => {
+        const { top: calendarTop, left: calendarLeft } = calendar.getBoundingClientRect();
+        const { left, right, top, bottom, width, height } = cell.getBoundingClientRect();
+        const { width: modalWidth, height: modalHeight } = modal.getBoundingClientRect();
+
+        // calculates the position of the clicked cell in the calendar
+        const cellPos = {
+            xPos: (right - calendarLeft) / width,
+            yPos: (bottom - calendarTop) / height,
+        };
+
+        const pos: typeof modalInfo = {};
+
+        // if the cell is the first 3 horiontally then render on the right else on the left
+        if (cellPos.xPos < 4) {
+            pos.left = right + 10;
+        } else {
+            pos.left = left - modalWidth;
+        }
+
+        // position the top so that it doesn't go outside the calendar and appears distinct when opened
+        if (cellPos.yPos < 4) {
+            pos.top = top + 10;
+        } else if (cellPos.yPos < 5) {
+            pos.top = top - modalHeight / 2;
+        } else if (cellPos.yPos < 7) {
+            pos.top = top - modalHeight / 4;
+        } else {
+            pos.top = top - modalHeight / 1.5;
+        }
+
+        return pos;
     };
+
+    const handleDateClick = ({ dayEl }: { dayEl: HTMLElement }): void => {
+        if (calendarRef.current && modalRef.current) {
+            setModalInfo(calculateModalPos(calendarRef.current.getApi().el, dayEl, modalRef.current));
+        }
+    };
+
     return (
         <Wrapper>
-            <Modal position={modalInfo} />
-            <StyledCalendar dateClick={handleDateClick} />
+            <Modal ref={modalRef} position={modalInfo} />
+            <Calendar ref={calendarRef} dateClick={handleDateClick} />
         </Wrapper>
     );
 };
@@ -26,14 +61,5 @@ const Dashboard: React.FC<{}> = () => {
 const Wrapper = styled.div`
     ${tw`flex h-full`}
 `;
-
-const StyledCalendar = styled(Calendar)`
-    ${tw`flex-1 lg:border-r-2`}
-`;
-
-// @TODO: add new sidebar
-// const StyledSidebar = styled(Sidebar)`
-//     ${tw`flex-0 w-3/12`}
-// `;
 
 export default Dashboard;
