50a876e97d6db0184258002d7ec3ab93153d79e3
lawrencek0 <l.khadka@outlook.com>
2019-11-20 21:35:46 -0600

    add form for creating appointment to the modal

--- src/calendar/dashboard/Dashboard.tsx
+++ src/calendar/dashboard/Dashboard.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useRef } from 'react';
 import tw from 'tailwind.macro';
 import styled from 'styled-components/macro';
-import Calendar from 'calendar/dashboard/Calendar';
-import { Modal } from './Modal';
+import moment from 'moment';
 import FullCalendar from '@fullcalendar/react';
+import { DateClickApi } from '@fullcalendar/core/Calendar';
+import Calendar from 'calendar/dashboard/Calendar';
+import { Modal, Props as ModalProps, Position } from './Modal';
 
 const Dashboard: React.FC<{}> = () => {
-    const [modalInfo, setModalInfo] = useState<{ top?: number; left?: number }>({});
+    const [modalInfo, setModalInfo] = useState<Pick<ModalProps, 'position' | 'startDate'>>({});
     const calendarRef = useRef<FullCalendar>(null);
     const modalRef = useRef<HTMLElement>(null);
 
-    const calculateModalPos = (calendar: HTMLElement, cell: HTMLElement, modal: HTMLElement): typeof modalInfo => {
+    const calculateModalPos = (calendar: HTMLElement, cell: HTMLElement, modal: HTMLElement): Position => {
         const { top: calendarTop, left: calendarLeft } = calendar.getBoundingClientRect();
         const { left, right, top, bottom, width, height } = cell.getBoundingClientRect();
         const { width: modalWidth, height: modalHeight } = modal.getBoundingClientRect();
@@ -21,38 +23,46 @@ const Dashboard: React.FC<{}> = () => {
             yPos: (bottom - calendarTop) / height,
         };
 
-        const pos: typeof modalInfo = {};
+        const pos: Position = {};
 
         // if the cell is the first 3 horiontally then render on the right else on the left
         if (cellPos.xPos < 4) {
             pos.left = right + 10;
         } else {
-            pos.left = left - modalWidth;
+            pos.left = left - modalWidth - 10;
         }
 
         // position the top so that it doesn't go outside the calendar and appears distinct when opened
         if (cellPos.yPos < 4) {
             pos.top = top + 10;
         } else if (cellPos.yPos < 5) {
-            pos.top = top - modalHeight / 2;
+            pos.top = top - modalHeight * 0.5;
         } else if (cellPos.yPos < 7) {
-            pos.top = top - modalHeight / 4;
+            pos.top = top - modalHeight * 0.8;
         } else {
-            pos.top = top - modalHeight / 1.5;
+            pos.top = top - modalHeight * 0.9;
         }
 
         return pos;
     };
 
-    const handleDateClick = ({ dayEl }: { dayEl: HTMLElement }): void => {
+    const handleDateClick = ({ dayEl, date }: DateClickApi): void => {
         if (calendarRef.current && modalRef.current) {
-            setModalInfo(calculateModalPos(calendarRef.current.getApi().el, dayEl, modalRef.current));
+            const position = calculateModalPos(calendarRef.current.getApi().el, dayEl, modalRef.current);
+            setModalInfo({
+                position,
+                startDate: moment(date)
+                    .hours(moment().hours())
+                    .minute(moment().minutes())
+                    .second(0)
+                    .millisecond(0),
+            });
         }
     };
 
     return (
         <Wrapper>
-            <Modal ref={modalRef} position={modalInfo} />
+            <Modal ref={modalRef} {...modalInfo} />
             <Calendar ref={calendarRef} dateClick={handleDateClick} />
         </Wrapper>
     );
--- src/calendar/dashboard/Modal.tsx
+++ src/calendar/dashboard/Modal.tsx
@@ -1,45 +1,152 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import moment from 'moment';
 import { Link } from '@reach/router';
-import { FaUserFriends } from 'react-icons/fa';
+import { FaHeading } from 'react-icons/fa';
+import { FaAlignLeft } from 'react-icons/fa';
+import { FaCalendarWeek } from 'react-icons/fa';
+import { FaClock } from 'react-icons/fa';
+import { FaUsers } from 'react-icons/fa';
+import { PrimaryButton, FlatButton } from 'shared/buttons';
 
-type Props = {
-    position?: { left?: number; top?: number };
+type EventType = 'appointment';
+
+export type Position = { left?: number; top?: number };
+
+export type Props = {
+    position?: Position;
+    type?: EventType;
+    startDate?: moment.Moment;
 };
 
-export const Modal = forwardRef<HTMLElement, Props>(({ position }, ref) => {
-    const handleSubmit = (): void => {
-        /* TODO */
-    };
-    return createPortal(
-        <Wrapper ref={ref} aria-modal={true} tabIndex={-1} {...position}>
-            <Formik initialValues={{}} onSubmit={handleSubmit}>
-                <Form>
-                    <StyledLink to="./appointments/new/1">
-                        <StyledIcon
-                            as={FaUserFriends}
-                            aria-label="Create Appointment"
-                            aria-labelledby="create-appointment"
-                        />
-                        <span id="create-appointment">Appointment</span>
-                    </StyledLink>
-                </Form>
-            </Formik>
-        </Wrapper>,
-        document.body,
-    );
-});
+export const Modal = forwardRef<HTMLElement, Props>(
+    ({ position, type: eventType = 'appointment', startDate = moment() }, ref) => {
+        const [type, setType] = useState<EventType>(eventType);
+
+        const handleTypeClick = ({ currentTarget }: React.MouseEvent<HTMLButtonElement>): void => {
+            const value = currentTarget.name as EventType;
+            setType(value);
+        };
+
+        const handleSubmit = (): void => {
+            /* TODO */
+        };
+        return createPortal(
+            <Wrapper ref={ref} aria-modal={true} tabIndex={-1} {...position}>
+                <Formik
+                    initialValues={{
+                        title: '',
+                        description: '',
+                        startDate: moment(startDate).format('YYYY-MM-DD'),
+                        endDate: moment(startDate).format('YYYY-MM-DD'),
+                        startTime: moment(startDate).format('HH:mm'),
+                        endTime: moment(startDate)
+                            .add(1, 'h')
+                            .format('HH:mm'),
+                    }}
+                    enableReinitialize
+                    onSubmit={handleSubmit}
+                >
+                    {({ values }) => (
+                        <StyledForm autoComplete="off">
+                            <StyledIcon as={FaHeading} aria-hidden />
+                            <StyledField
+                                css={tw`text-2xl`}
+                                name="title"
+                                placeholder="Enter the title"
+                                aria-label="Title"
+                                innerRef={(el: HTMLInputElement) => el && el.focus()}
+                                required
+                            />
+                            <ButtonWrapper>
+                                <StyledButton
+                                    css={tw`bg-gray-200`}
+                                    type="button"
+                                    name="appointments"
+                                    onClick={handleTypeClick}
+                                    active={type === 'appointment'}
+                                >
+                                    Appointment
+                                </StyledButton>
+                            </ButtonWrapper>
+                            <Separator />
+                            <StyledIcon as={FaCalendarWeek} aria-hidden />
+                            <div css={tw`flex`}>
+                                <StyledField css={tw`mr-4`} type="date" name="startDate" aria-label="Start date" />
+                                <StyledField min={values.startDate} type="date" name="endDate" aria-label="End date" />
+                            </div>
+                            <StyledIcon as={FaClock} aria-hidden />
+                            <div css={tw`flex`}>
+                                <StyledField css={tw`mr-4`} type="time" name="startTime" aria-label="Start time" />
+                                <StyledField min={values.startTime} type="time" name="endTime" aria-label="End time" />
+                            </div>
+                            <Separator />
+                            <StyledIcon as={FaUsers} aria-hidden />
+                            <StyledField aria-label="Attendees" disabled value="TODO" />
+                            <Separator />
+                            <StyledIcon as={FaAlignLeft} aria-hidden />
+                            <StyledField
+                                as="textarea"
+                                name="description"
+                                placeholder="Enter the description"
+                                aria-label="Description"
+                            />
+                            <ActionButtons>
+                                <FlatButton css={tw`mr-4`} type="button">
+                                    More Options
+                                </FlatButton>
+                                <PrimaryButton type="submit">Submit</PrimaryButton>
+                            </ActionButtons>
+                        </StyledForm>
+                    )}
+                </Formik>
+            </Wrapper>,
+            document.body,
+        );
+    },
+);
 
 Modal.displayName = 'Modal';
 
 const Wrapper = styled.aside<{ left?: number; top?: number }>`
-    ${tw`absolute bg-white rounded w-2/12 h-32 px-8 py-4 text-center z-10 shadow-lg`};
+    ${tw`absolute bg-white rounded lg:w-4/12 xl:w-3/12 p-4 z-10 shadow-lg`};
     visibility: ${props => (props.left ? 'shown' : 'hidden')};
     left: ${props => `${props.left}px`};
     top: ${props => (props.top ? `${props.top}px` : 0)};
+`;
+
+const StyledForm = styled(Form)`
+    display: grid;
+    grid-template-columns: minmax(min-content, 36px) 1fr;
+    grid-gap: 1em 0.5em;
+    align-items: center;
+`;
+
+const StyledField = styled(Field)`
+    ${tw`hover:bg-gray-300 focus:bg-gray-300 px-2 pt-2 w-full focus:border-b-2 border-gray-800`}
+`;
+
+const ButtonWrapper = styled.div`
+    ${tw`flex`}
+    grid-column-start: 2;
+`;
+
+const StyledButton = styled(FlatButton)<{ active: boolean }>`
+    ${props => props.active && tw`bg-gray-200`};
+`;
+
+const ActionButtons = styled.div`
+    ${tw`flex justify-end`}
+    grid-column-start: 2;
+`;
+
+const Separator = styled.div`
+    ${tw`bg-gray-200 w-full`}
+    grid-column: span 2;
+    height: 2px;
 `;
 
 const StyledLink = styled(Link)`
@@ -47,5 +154,5 @@ const StyledLink = styled(Link)`
 `;
 
 const StyledIcon = styled.div`
-    ${tw`text-5xl`}
+    ${tw`text-xl`}
 `;
