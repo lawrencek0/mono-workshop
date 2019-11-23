26fb1e9de559d24fc7a047b5ff8b3279efc1f101
lawrencek0 <l.khadka@outlook.com>
2019-11-22 23:44:36 -0600

    reset cache on logout

--- src/auth/Logout.tsx
+++ src/auth/Logout.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { useAuthDispatch } from 'auth/hooks';
+import { useResetter } from 'rest-hooks';
 
 const Logout: React.FC<RouteComponentProps> = () => {
+    const resetCache = useResetter();
     const dispatch = useAuthDispatch();
+    resetCache();
     dispatch({ type: 'logout' });
 
     // @TODO: instead show a popup/notification
