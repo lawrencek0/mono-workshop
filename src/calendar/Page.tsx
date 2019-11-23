aaa1077060f72081231f30e2693e7d861af7def5
lawrencek0 <l.khadka@outlook.com>
2019-11-23 14:32:04 -0600

    add NetworkErrorBoundary

--- src/calendar/Page.tsx
+++ src/calendar/Page.tsx
@@ -1,7 +1,8 @@
 import React, { lazy, Suspense } from 'react';
 import { RouteComponentProps, Router } from '@reach/router';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import tw from 'tailwind.macro';
+import { NetworkErrorBoundary, NetworkError } from 'rest-hooks';
 import { Main } from 'navigation/Main';
 import { RouteGuard } from 'routing/PrivateRoute';
 import Dashboard from './dashboard/Dashboard';
@@ -9,21 +10,37 @@ import Select from './appointment/Select';
 
 const AppointmentCreationForm = lazy(() => import('./appointment/create/Form'));
 
+const ErrorComponent: React.FC<{ error: NetworkError }> = ({ error }) => {
+    return (
+        <div css={tw`max-w-4xl m-auto mt-8`}>
+            <p>
+                <strong>Oops! An error occured!</strong>
+            </p>
+            <div>
+                {error.status}: {error.message}
+            </div>
+            {error.response && <pre css={tw`whitespace-pre-line`}>{JSON.stringify(error.response.body)}</pre>}
+        </div>
+    );
+};
+
 const Calendar: React.FC<RouteComponentProps> = () => {
     return (
         <Main>
             <Suspense fallback={<div>Loading events...</div>}>
-                <StyledRouter>
-                    <RouteGuard as={Dashboard} action="events:visit" path="/"></RouteGuard>
-                    {/* @TODO: add dynamic RBAC rules? */}
-                    <RouteGuard as={Select} action="events:visit" path="appointments/:detailId"></RouteGuard>
-                    {/* @FIXME: use query params instead? */}
-                    <RouteGuard
-                        as={AppointmentCreationForm}
-                        action="events:add"
-                        path="appointments/new/:step"
-                    ></RouteGuard>
-                </StyledRouter>
+                <NetworkErrorBoundary fallbackComponent={ErrorComponent}>
+                    <StyledRouter>
+                        <RouteGuard as={Dashboard} action="events:visit" path="/"></RouteGuard>
+                        {/* @TODO: add dynamic RBAC rules? */}
+                        <RouteGuard as={Select} action="events:visit" path="appointments/:detailId"></RouteGuard>
+                        {/* @FIXME: use query params instead? */}
+                        <RouteGuard
+                            as={AppointmentCreationForm}
+                            action="events:add"
+                            path="appointments/new/:step"
+                        ></RouteGuard>
+                    </StyledRouter>
+                </NetworkErrorBoundary>
             </Suspense>
         </Main>
     );
