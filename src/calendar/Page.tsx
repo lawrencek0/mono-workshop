c6dd1fa63c25cd387181710caadee606d442cd82
lawrencek0 <l.khadka@outlook.com>
2019-11-22 20:08:14 -0600

    add suspense boundary to calendar

--- src/calendar/Page.tsx
+++ src/calendar/Page.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { RouteComponentProps, Router } from '@reach/router';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
@@ -12,13 +12,19 @@ const AppointmentCreationForm = lazy(() => import('./appointment/create/Form'));
 const Calendar: React.FC<RouteComponentProps> = () => {
     return (
         <Main>
-            <StyledRouter>
-                <RouteGuard as={Dashboard} action="events:visit" path="/"></RouteGuard>
-                {/* @TODO: add dynamic RBAC rules? */}
-                <RouteGuard as={Select} action="events:visit" path="appointments/:detailId"></RouteGuard>
-                {/* @FIXME: use query params instead? */}
-                <RouteGuard as={AppointmentCreationForm} action="events:add" path="appointments/new/:step"></RouteGuard>
-            </StyledRouter>
+            <Suspense fallback={<div>Loading events...</div>}>
+                <StyledRouter>
+                    <RouteGuard as={Dashboard} action="events:visit" path="/"></RouteGuard>
+                    {/* @TODO: add dynamic RBAC rules? */}
+                    <RouteGuard as={Select} action="events:visit" path="appointments/:detailId"></RouteGuard>
+                    {/* @FIXME: use query params instead? */}
+                    <RouteGuard
+                        as={AppointmentCreationForm}
+                        action="events:add"
+                        path="appointments/new/:step"
+                    ></RouteGuard>
+                </StyledRouter>
+            </Suspense>
         </Main>
     );
 };
