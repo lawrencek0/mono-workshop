59ab37a223e358f794efa53377168475189a1a04
lawrencek0 <l.khadka@outlook.com>
2019-11-24 21:42:35 -0600

    add post creation page

--- src/groups/Page.tsx
+++ src/groups/Page.tsx
@@ -1,11 +1,13 @@
 import React, { Suspense, lazy } from 'react';
 import { RouteComponentProps, Router } from '@reach/router';
+import 'react-tiny-fab/dist/styles.css';
 import { Main } from 'navigation/Main';
 import { RouteGuard } from 'routing/PrivateRoute';
 import { Dashboard } from './Dashboard';
 import { Group } from './group/Page';
 
 const GroupCreationForm = lazy(() => import('./Create'));
+const PostCreationFrom = lazy(() => import('./posts/Create'));
 
 const Page: React.FC<RouteComponentProps> = () => {
     return (
@@ -14,6 +16,7 @@ const Page: React.FC<RouteComponentProps> = () => {
                 <Router>
                     <RouteGuard as={Dashboard} path="/" action="groups:visit" />
                     <RouteGuard as={GroupCreationForm} path="/new" action="groups:create" />
+                    <RouteGuard as={PostCreationFrom} path="/posts/new" action="groups:visit" />
                     <Group path=":groupId/*" />
                 </Router>
             </Suspense>
