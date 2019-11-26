47b1bb2356dcd0cb402974dbf45e1e1a6b1bd7c0
alexLau <alexlau2014@gmail.com>
2019-11-25 21:37:01 -0600

    changed Put to Patch
    deleted some unused bits of code

--- src/api/events/controller.ts
+++ src/api/events/controller.ts
@@ -7,9 +7,9 @@ import {
     Get,
     Param,
     Delete,
-    Put,
     UnauthorizedError,
     BodyParam,
+    Patch,
 } from 'routing-controllers';
 import { User } from '../users/entity/User';
 import { Event } from './entity/Event';
@@ -17,7 +17,7 @@ import { Inject } from 'typedi';
 import { EventRepository, EventRosterRepository } from './repository';
 import { UserRepository } from '../users/repository';
 import { GroupEventRoster } from '../groups/entity/GroupEventRoster';
-import { GroupEventRepository, GroupRepository, GroupUsersRepository } from '../groups/repository';
+import { GroupEventRepository } from '../groups/repository';
 import { getRepository } from 'typeorm';
 import { EventRoster } from './entity/EventRoster';
 
@@ -26,9 +26,7 @@ export class EventController {
     @Inject() private eventRepository: EventRepository;
     @Inject() private eventRosterRepository: EventRosterRepository;
     @Inject() private userRepository: UserRepository;
-    @Inject() private groupRepo: GroupRepository;
     @Inject() private groupEventsRepo: GroupEventRepository;
-    @Inject() private groupUsersRepo: GroupUsersRepository;
 
     @Get('/')
     async findAll(@CurrentUser({ required: true }) user: User) {
@@ -86,7 +84,7 @@ export class EventController {
         }
     }
 
-    @Put('/:id')
+    @Patch('/:id')
     async update(@CurrentUser({ required: true }) user: User, @Body() event: Event, @Param('id') id: number) {
         try {
             const currentEvent: Event = await this.eventRepository.findById(id);
