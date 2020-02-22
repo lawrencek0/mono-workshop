7305105bb209dcd2b01c02ab27055a442812d164
lawrencek0 <l.khadka@outlook.com>
2020-02-21 22:11:44 -0600

    use sequenced phage endpoint to download phages instead

--- src/phagesdb/api.rs
+++ src/phagesdb/api.rs
@@ -1,12 +1,9 @@
-use futures::{future, stream, StreamExt};
-use rusqlite::{params, Connection};
+use futures::{stream, StreamExt};
 use serde::Deserialize;
 use tempfile::Builder;
 use tokio::fs::File;
 use tokio::prelude::*;
-use tokio::task::spawn_blocking;
-
-use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
 
 use super::Phage;
 #[derive(Deserialize, Debug)]
@@ -18,53 +15,29 @@ struct HostGenus {
 #[derive(Deserialize, Debug)]
 struct PhageList {
     count: u32,
-    results: Vec<Phage>,
-    next: Option<String>,
+    #[serde(alias = "results")]
+    phages: Vec<Phage>,
 }
 
-pub async fn get_host_genera() -> Result<Vec<HostGenus>, reqwest::Error> {
-    reqwest::get("https://phagesdb.org/api/host_genera/")
+pub async fn get_phages(mut tx: mpsc::Sender<Phage>) -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://phagesdb.org/api/sequenced_phages/?page_size=1")
         .await?
-        .json::<Vec<HostGenus>>()
-        .await
-}
-
-pub async fn get_phages(genus: u8) -> Result<Vec<Phage>, reqwest::Error> {
-    let mut res = reqwest::get(&format!(
-        "https://phagesdb.org/api/host_genera/{}/phagelist/?page=1",
-        genus
+        .json::<PhageList>()
+        .await?;
+    let res = reqwest::get(&format!(
+        "https://phagesdb.org/api/sequenced_phages/?page_size={}",
+        res.count
     ))
     .await?
     .json::<PhageList>()
     .await?;
 
-    if res.next.is_none() {
-        return Ok(res
-            .results
-            .into_iter()
-            .filter(|p| p.fasta_file.is_some())
-            .collect());
+    for phage in res.phages.into_iter() {
+        tx.send(phage).await.expect("receiver dropped");
     }
 
-    let mut phages = res.results;
 
-    loop {
-        res = reqwest::get(res.next.as_ref().unwrap())
-            .await?
-            .json::<PhageList>()
-            .await?;
-
-        phages.extend(
-            res.results
-                .into_iter()
-                .filter(|p| p.fasta_file.is_some())
-                .collect::<Vec<Phage>>(),
-        );
-
-        if res.next.is_none() {
-            return Ok(phages);
-        }
-    }
+    Ok(())
 }
 
 pub async fn download_fasta_files(
@@ -88,43 +61,4 @@ pub async fn download_fasta_files(
         .await;
 
     Ok(tmp_dir)
-}
-
-pub async fn update_phagesdb(
-    conn: Arc<Mutex<Connection>>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let genera = get_host_genera().await?;
-    let phage_lists = stream::iter(genera)
-        .map(|genus| async move { get_phages(genus.id).await })
-        .buffer_unordered(8);
-    phage_lists
-        .for_each(move |phages| {
-            let conn = conn.clone();
-            spawn_blocking(move || {
-                let phages = phages.unwrap();
-                phages
-                    .iter()
-                    .filter(|phage| phage.fasta_file.is_some())
-                    .for_each(|phage| {
-                        let conn = conn.lock().unwrap();
-                        conn.execute(
-                            "INSERT OR IGNORE INTO phagesdb 
-                        (name, genus, cluster, subcluster, endType, fastaFile) 
-                        VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-                            params!(
-                                phage.name,
-                                phage.genus,
-                                phage.cluster,
-                                phage.subcluster,
-                                phage.end_type.as_ref().map(|s| s.to_string()),
-                                phage.fasta_file.as_ref().map(|s| s.to_string()),
-                            ),
-                        )
-                        .unwrap();
-                    });
-            });
-            future::ready(())
-        })
-        .await;
-    Ok(())
 }
