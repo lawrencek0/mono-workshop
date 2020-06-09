bcb6beee30b6cac4ce138672cf8aa3b7dd3e75d0
lawrencek0 <l.khadka@outlook.com>
2020-06-08 20:33:08 -0500

    read from stdin in separate thread and make stream nonblocking

--- src/telnet/client.rs
+++ src/telnet/client.rs
@@ -1,7 +1,8 @@
 use std::convert::TryFrom;
-use std::io::prelude::*;
-use std::io::{self, BufReader, BufWriter};
+use std::io::{self, prelude::*, BufReader, BufWriter};
 use std::net::TcpStream;
+use std::sync::mpsc::{self, Receiver};
+use std::thread;
 
 use super::{Command, Option, TerminalType};
 
@@ -14,11 +15,14 @@ pub struct Client {
     input: BufReader<TcpStream>,
     output: BufWriter<TcpStream>,
     configuration: Configuration,
+    input_channel: Receiver<(usize, Vec<u8>)>,
 }
 
 impl Client {
     pub fn new(addr: &str) -> io::Result<Self> {
         let stream = TcpStream::connect(addr)?;
+        stream.set_nonblocking(true).unwrap();
+
         let configuration = Configuration {
             should_echo: false,
             should_supress_ga: false,
@@ -28,124 +32,145 @@ impl Client {
             input: BufReader::new(stream.try_clone()?),
             output: BufWriter::new(stream),
             configuration,
+            input_channel: spawn_stdin_channel(),
         })
+    }
+
+    pub fn run(&mut self) -> io::Result<()> {
+        loop {
+            if let Ok((n, input)) = self.input_channel.try_recv() {
+                self.output.write_all(&input[..n])?;
+            }
+
+            self.process()?;
+            self.output.flush()?;
+        }
+
+        Ok(())
     }
 
     pub fn process(&mut self) -> io::Result<()> {
         let mut buf = [0; 2048];
 
-        let n = self.input.read(&mut buf[..])?;
+        if let Ok(n) = self.input.read(&mut buf[..]) {
+            // request more bytes if its an IAC
+            if n == 1 && buf[0] == Command::IAC.into() {
+                loop {
+                    match self.input.read(&mut buf[1..]) {
+                        Ok(_) => break,
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                        Err(e) => return Err(e),
+                    }
+                }
+            }
 
-        // request more bytes if its an IAC
-        if n == 1 && buf[0] == Command::IAC.into() {
-            self.input.read(&mut buf[1..])?;
-        }
+            let mut iter = buf.iter().take_while(|b| **b != 0);
 
-        let mut iter = buf.iter().take_while(|b| **b != 0);
+            let mut stdout = BufWriter::new(io::stdout());
 
-        let stdout = io::stdout();
-        let mut stdout = BufWriter::new(stdout.lock());
-
-        while let Some(byte) = iter.next() {
-            match Command::try_from(*byte) {
-                Ok(Command::IAC) => {
-                    match Command::try_from(
-                        *iter
-                            .next()
-                            .unwrap_or_else(|| panic!("no actual command! {}",)),
-                    )
-                    .unwrap()
-                    {
-                        Command::WILL => {
-                            let opt = Option::try_from(*iter.next().unwrap()).unwrap();
-                            match opt {
-                                Option::SuppressGA => {
-                                    self.configuration.should_supress_ga = true;
-                                    self.output.write_all(&[
-                                        Command::IAC.into(),
-                                        Command::DO.into(),
-                                        Option::SuppressGA.into(),
-                                    ])?;
-                                }
-                                Option::Echo => {
-                                    self.configuration.should_echo = true;
-                                    // maybe change writer from bufreader to smth else?
-                                    self.send_command(&[Command::DO.into(), Option::Echo.into()])?;
-                                }
-                                _ => unimplemented!("opt for will {:?}", opt),
-                            }
-                        }
-                        Command::DO => {
-                            let opt = Option::try_from(*iter.next().unwrap()).unwrap();
-                            match opt {
-                                Option::TerminalType => {
-                                    // TODO: then what?
-                                    self.output.write_all(&[
-                                        Command::IAC.into(),
-                                        Command::WILL.into(),
-                                        Option::TerminalType.into(),
-                                    ])?;
-                                }
-                                Option::NAWS => {
-                                    // TODO: get actual width nd height
-                                    let (width, height) =
-                                        (174u16.to_be_bytes(), 48u16.to_be_bytes());
-
-                                    self.output.write_all(&[
-                                        Command::IAC.into(),
-                                        Command::WILL.into(),
-                                        Option::NAWS.into(),
-                                        // subneg time
-                                        Command::IAC.into(),
-                                        Command::SB.into(),
-                                        width[0],
-                                        width[1],
-                                        height[0],
-                                        height[1],
-                                        Command::IAC.into(),
-                                        Command::SE.into(),
-                                    ])?;
-                                }
-                                _ => unimplemented!("opt for do {:?}", opt),
-                            }
-                        }
-                        Command::SB => {
-                            let opt = Option::try_from(*iter.next().unwrap()).unwrap();
-                            match opt {
-                                Option::TerminalType => {
-                                    if TerminalType::try_from(*iter.next().unwrap()).unwrap()
-                                        == TerminalType::SEND
-                                    {
+            while let Some(byte) = iter.next() {
+                match Command::try_from(*byte) {
+                    Ok(Command::IAC) => {
+                        match Command::try_from(
+                            *iter
+                                .next()
+                                .unwrap_or_else(|| panic!("no actual command! {}",)),
+                        )
+                        .unwrap()
+                        {
+                            Command::WILL => {
+                                let opt = Option::try_from(*iter.next().unwrap()).unwrap();
+                                match opt {
+                                    Option::SuppressGA => {
+                                        self.configuration.should_supress_ga = true;
                                         self.output.write_all(&[
                                             Command::IAC.into(),
-                                            Command::SB.into(),
-                                            Option::TerminalType.into(),
-                                            TerminalType::IS.into(),
+                                            Command::DO.into(),
+                                            Option::SuppressGA.into(),
                                         ])?;
-                                        self.output.write(b"xterm-256color")?;
+                                    }
+                                    Option::Echo => {
+                                        self.configuration.should_echo = true;
+                                        // maybe change writer from bufreader to smth else?
+                                        self.send_command(&[
+                                            Command::DO.into(),
+                                            Option::Echo.into(),
+                                        ])?;
+                                    }
+                                    _ => unimplemented!("opt for will {:?}", opt),
+                                }
+                            }
+                            Command::DO => {
+                                let opt = Option::try_from(*iter.next().unwrap()).unwrap();
+                                match opt {
+                                    Option::TerminalType => {
+                                        // TODO: then what?
                                         self.output.write_all(&[
+                                            Command::IAC.into(),
+                                            Command::WILL.into(),
+                                            Option::TerminalType.into(),
+                                        ])?;
+                                    }
+                                    Option::NAWS => {
+                                        // TODO: get actual width nd height
+                                        let (width, height) =
+                                            (174u16.to_be_bytes(), 48u16.to_be_bytes());
+
+                                        self.output.write_all(&[
+                                            Command::IAC.into(),
+                                            Command::WILL.into(),
+                                            Option::NAWS.into(),
+                                            // subneg time
+                                            Command::IAC.into(),
+                                            Command::SB.into(),
+                                            width[0],
+                                            width[1],
+                                            height[0],
+                                            height[1],
                                             Command::IAC.into(),
                                             Command::SE.into(),
                                         ])?;
-                                        assert_eq!(iter.next().unwrap(), &Command::IAC.into());
-                                        assert_eq!(iter.next().unwrap(), &Command::SE.into());
                                     }
+                                    _ => unimplemented!("opt for do {:?}", opt),
                                 }
-                                _ => unimplemented!(),
                             }
+                            Command::SB => {
+                                let opt = Option::try_from(*iter.next().unwrap()).unwrap();
+                                match opt {
+                                    Option::TerminalType => {
+                                        if TerminalType::try_from(*iter.next().unwrap()).unwrap()
+                                            == TerminalType::SEND
+                                        {
+                                            self.output.write_all(&[
+                                                Command::IAC.into(),
+                                                Command::SB.into(),
+                                                Option::TerminalType.into(),
+                                                TerminalType::IS.into(),
+                                            ])?;
+                                            self.output.write(b"xterm-256color")?;
+                                            self.output.write_all(&[
+                                                Command::IAC.into(),
+                                                Command::SE.into(),
+                                            ])?;
+                                            assert_eq!(iter.next().unwrap(), &Command::IAC.into());
+                                            assert_eq!(iter.next().unwrap(), &Command::SE.into());
+                                        }
+                                    }
+                                    _ => unimplemented!(),
+                                }
+                            }
+                            _ => unimplemented!("not implemented command"),
                         }
-                        _ => unimplemented!("not implemented command"),
+                    }
+                    Ok(c) => unimplemented!("Command {:?} coming soon", c),
+                    Err(_) => {
+                        stdout.write(&[*byte])?;
                     }
                 }
-                Ok(c) => unimplemented!("Command {:?} coming soon", c),
-                Err(_) => {
-                    stdout.write(&[*byte])?;
-                }
             }
-        }
 
-        self.output.flush()?;
-        stdout.flush()?;
+            stdout.flush()?;
+        }
 
         Ok(())
     }
@@ -154,4 +179,14 @@ impl Client {
         self.output
             .write_all(&[&[Command::IAC.into()], cmd].concat())
     }
+}
+
+fn spawn_stdin_channel() -> mpsc::Receiver<(usize, Vec<u8>)> {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || loop {
+        let mut buf = vec![0; 15];
+        let n = io::stdin().read(&mut buf).unwrap();
+        tx.send((n, buf)).unwrap();
+    });
+    rx
 }
