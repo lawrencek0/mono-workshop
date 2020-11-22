5f23814a6da9e41939da9eda17ea9db94797646c
Lawrence Khadka <l.khadka@outlook.com>
2020-11-22 14:27:38 -0700

    create base class variables

--- queues/src/Deque.java
+++ queues/src/Deque.java
@@ -1,17 +1,32 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
+    private Node<Item> head;
+    private Node<Item> tail;
+    private int size;
+
+    private class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+
+        private Node(Item i) {
+            this.item = i;
+        }
+    }
 
     // construct an empty deque
     public Deque() {
+        this.size = 0;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
+        return this.size == 0;
     }
 
     // return the number of items on the deque
     public int size() {
+        return this.size;
     }
 
     // add the item to the front
